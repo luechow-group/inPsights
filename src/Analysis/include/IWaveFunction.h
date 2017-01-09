e76ddf3a83af5ed3dccfc695ddca247b71da3b6c
Michael Andree Heuer <mheuer@student.ethz.ch>
2017-01-09 16:43:11 +0100

    Added Analysis project skeleton.

---
+++ src/Analysis/include/IWaveFunction.h
@@ -0,0 +1,8 @@
+//
+// Created by Michael Heuer on 09.01.17.
+//
+
+#ifndef AMOLQCGUI_IWAVEFUNCTION_H
+#define AMOLQCGUI_IWAVEFUNCTION_H
+
+#endif //AMOLQCGUI_IWAVEFUNCTION_H
---
+++ src/Analysis/source/IWaveFunction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michael Heuer on 09.01.17.
+//
+
+#include "IWaveFunction.h"
