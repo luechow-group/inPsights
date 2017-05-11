3d4d4cb14e9b81c01d93c5fc110b333251cefa5e
Unknown <michael.heuer@rwth-aachen.de>
2017-05-11 17:01:52 +0200

    Added features to the main to test the electron implementation.

--- src/GUI/main.cpp
+++ src/GUI/main.cpp
@@ -15,84 +15,97 @@
 #include "Electron.h"
 
 #include "BSpline.h"
+#include "StringMethod.h"
 
 
-Qt3DCore::QEntity* createTestScene()
-{
+Qt3DCore::QEntity *createTestScene() {
 
-    Qt3DCore::QEntity* root = new Qt3DCore::QEntity();
+  Eigen::VectorXd xA(2 * 3);
+  Eigen::VectorXd xB(2 * 3);
 
-    auto p1=QVector3D(10.0f, 0.0f, 4.0f);
-    auto p2=QVector3D(-5.0f, -10.0f, 3.0f);
-    auto p3=QVector3D(-0.0f, -10.0f, -5.0f);
+  xA << 0.0, 0.01, 0.70014273, 0.02, 0.09, -0.70014273;//0.0,0.00,+0.7,0.0,0.0,-0.7;
+  xB << 0.01, -0.0, +0.70014273, -0.05, 0.01, +0.70014273;//0.0,0.00,-0.7,0.0,0.0,+0.7;
 
-    Sphere s0(root, Qt::black, QVector3D(0, 0, 0), 0.5f);
+  unsigned numberOfStates = 15;
 
-    Cylinder x(root,Qt::red,{QVector3D(0,0,0),QVector3D(20,0,0)},.25f);
-    Cylinder y(root,Qt::green,{QVector3D(0,0,0),QVector3D(0,20,0)},.25f);
-    Cylinder z(root,Qt::blue,{QVector3D(0,0,0),QVector3D(0,0,20)},.25f);
+  Eigen::MatrixXd initialChain(2 * 3, numberOfStates);
 
-    Atom N(root,p1,Elements::ElementType::N);
-    Atom O(root,p2,Elements::ElementType::O);
-    Atom C(root,p3,Elements::ElementType::C);
+  Eigen::VectorXd delta = xB - xA;
+  for (int i = 0; i < numberOfStates; ++i) {
+    double rel = double(i) / double(numberOfStates - 1);
 
-    Electron e1(root,QVector3D(12.0f, 1.0f, 7.0f),Spin::SpinType::Alpha);
-    Electron e2(root,QVector3D( 9.0f, 1.2f, 7.0f),Spin::SpinType::Beta);
-
-    Bond b1(N,O);
-    Bond b2(C,O);
-    /* TODO introduce "default" constructors, setter and update methods, (more flexibility and clean code than
-     * constructer stuff -> can be also callesd in constructor) (contains only root)
-     * abstract3Dobjects and all derived types atoms, bonds etc */
-
-    /*
-     * sphere:
-     * sphere(bla bla, location)
-     *  :Abstract3Dobj(root)
-     * setPosition(location),
-     * setColor(),y
-     * */
-
-    //Qt3DRender::QObjectPicker(); // emits signals for you to handle
-    //Qt3DRender::QPickingSettings* pickingSettings = new Qt3DRender::QPickingSettings();
-    //pickingSettings->setPickMethod(Qt3DRender::QPickingSettings::PickMethod::BoundingVolumePicking);
+    initialChain.col(i) = xA + (delta * rel);
+  }
 
 
-    return root;
+  StringMethod stringMethod(initialChain);
+
+  std::cout << stringMethod.getChain().coordinates() << std::endl;
+
+  stringMethod.optimizeString();
+
+  Qt3DCore::QEntity *root = new Qt3DCore::QEntity();
+
+  auto p1 = QVector3D(10.0f, 0.0f, 4.0f);
+  auto p2 = QVector3D(-5.0f, -10.0f, 3.0f);
+  auto p3 = QVector3D(-0.0f, -10.0f, -5.0f);
+
+  Sphere s0(root, Qt::black, QVector3D(0, 0, 0), 0.5f);
+
+  Cylinder x(root, Qt::red, {QVector3D(0, 0, 0), QVector3D(20, 0, 0)}, .25f);
+  Cylinder y(root, Qt::green, {QVector3D(0, 0, 0), QVector3D(0, 20, 0)}, .25f);
+  Cylinder z(root, Qt::blue, {QVector3D(0, 0, 0), QVector3D(0, 0, 20)}, .25f);
+
+  Atom N(root, p1, Elements::ElementType::N);
+  Atom O(root, p2, Elements::ElementType::O);
+  Atom C(root, p3, Elements::ElementType::C);
+
+  Electron e1(root, QVector3D(12.0f, 1.0f, 7.0f), Spin::SpinType::Alpha);
+  Electron e2(root, QVector3D(9.0f, 1.2f, 7.0f), Spin::SpinType::Beta);
+
+  Bond b1(N, O);
+  Bond b2(C, O);
+
+
+  //Qt3DRender::QObjectPicker(); // emits signals for you to handle
+  //Qt3DRender::QPickingSettings* pickingSettings = new Qt3DRender::QPickingSettings();
+  //pickingSettings->setPickMethod(Qt3DRender::QPickingSettings::PickMethod::BoundingVolumePicking);
+
+
+  return root;
 }
 
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
 
-    BSplines::BSpline bs;
-    
-    QGuiApplication app(argc, argv);
+  BSplines::BSpline bs;
 
-    Qt3DExtras::Qt3DWindow view;
-    Qt3DCore::QEntity* scene = createTestScene();
+  QGuiApplication app(argc, argv);
 
-    // camera
-    Qt3DRender::QCamera *camera = view.camera();
-    camera->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-    camera->setPosition(QVector3D(0, 0, 100.0f));
-    camera->setViewCenter(QVector3D(0, 0, 0));
-    //camera->setFarPlane(1000);
-    //camera->setNearPlane(0.1);
+  Qt3DExtras::Qt3DWindow view;
+  Qt3DCore::QEntity *scene = createTestScene();
 
-    // manipulator
-    //Qt3DExtras::QFirstPersonCameraController* manipulator = new Qt3DExtras::QFirstPersonCameraController(scene);
-    Qt3DExtras::QOrbitCameraController* manipulator = new Qt3DExtras::QOrbitCameraController(scene);
-    manipulator->setLinearSpeed(50.f);
-    manipulator->setLookSpeed(180.f);
-    manipulator->setCamera(camera);
+  // camera
+  Qt3DRender::QCamera *camera = view.camera();
+  camera->lens()->setPerspectiveProjection(45.0f, 16.0f / 9.0f, 0.1f, 1000.0f);
+  camera->setPosition(QVector3D(0, 0, 100.0f));
+  camera->setViewCenter(QVector3D(0, 0, 0));
+  //camera->setFarPlane(1000);
+  //camera->setNearPlane(0.1);
+
+  // manipulator
+  //Qt3DExtras::QFirstPersonCameraController* manipulator = new Qt3DExtras::QFirstPersonCameraController(scene);
+  Qt3DExtras::QOrbitCameraController *manipulator = new Qt3DExtras::QOrbitCameraController(scene);
+  manipulator->setLinearSpeed(50.f);
+  manipulator->setLookSpeed(180.f);
+  manipulator->setCamera(camera);
 
 
-    view.setRootEntity(scene);
+  view.setRootEntity(scene);
 
-    view.show();
+  view.show();
 
-    return app.exec();
+  return app.exec();
 
 
 }
