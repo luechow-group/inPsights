27aff00f82bccf686a8bea07335005f110941647
Morian Sonnet <MorianSonnet@googlemail.com>
2017-05-18 18:30:37 +0200

    Changed required Cmake Level to tested minimum. Changed String to string due to compilation problems at Linux

--- include/pse.h
+++ include/pse.h
@@ -4,7 +4,7 @@
 
 #ifndef LOCALSPINMULTIPLICITY_PSE_H
 #define LOCALSPINMULTIPLICITY_PSE_H
-#include <String>
+#include <string>
 
 std::string pse[]={"H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc",
                    "Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr","Nb",
