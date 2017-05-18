b91d6e2a2c74ad8a87af9e787567ca34e80efbc6
Morian Sonnet <MorianSonnet@googlemail.com>
2017-05-18 17:55:44 +0200

    Fixed Bugs: Project is compilable

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -15,6 +15,6 @@ double Particle::distance(const Particle &p1, const Particle &p2) {
     return sqrt(pow(p1.get_position()[0]-p2.get_position()[0],2)+pow(p1.get_position()[1]-p2.get_position()[1],2)+pow(p1.get_position()[2]-p2.get_position()[2],2));
 }
 
-const std::vector<double> &Particle::get_position()() const {
+const std::vector<double>& Particle::get_position() const {
     return this->position;
 }
