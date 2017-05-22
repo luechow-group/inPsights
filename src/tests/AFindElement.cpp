6692fc513866057756c3da420a10d5e5e8d07db2
Morian Sonnet <MorianSonnet@googlemail.com>
2017-05-22 14:09:36 +0200

    Solved Linking Problems for tests

---
+++ src/tests/AFindElement.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Moria on 22.05.2017.
+//
+
+#include "pse.h"
+#include "gtest/gtest.h"
+
+TEST(FindElementTest, ExistingElements)
+{
+    std::string testElements[11]= {"C", "H", "Xe", "Pb", "Kr", "Ni", "Cl", "At", "Ra", "Lu", "Hg"};
+    int testElementOZ[11]={6,1,54,82,36,28,17,85,88,71,80};
+    for(int i=0;i<11;i++){
+        EXPECT_EQ(testElementOZ[i],Pse::findElement(testElements[i]));
+    }
+}
+
+
---
+++ src/tests/ATestElectronAssigner.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Moria on 22.05.2017.
+//
+
