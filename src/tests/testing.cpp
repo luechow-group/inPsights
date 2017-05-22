744592938da056119f98e669b80562347b8d4f77
Morian Sonnet <MorianSonnet@googlemail.com>
2017-05-22 13:41:50 +0200

    Google test is usable, target LocalSpinMultiplicity runs all tests (not yet existent)

---
+++ src/tests/testing.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Moria on 22.05.2017.
+//
+#include <iostream>
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
