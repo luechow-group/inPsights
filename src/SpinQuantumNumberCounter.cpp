ed0b6374eaa1b6aebd1c5ff7cfcfebbea860cb28
Morian Sonnet <MorianSonnet@googlemail.com>
2017-05-29 11:36:57 +0200

    Added documentation to SpinQuantumNumberCounter

--- src/SpinQuantumNumberCounter.cpp
+++ src/SpinQuantumNumberCounter.cpp
@@ -7,8 +7,8 @@
 #include <iostream>
 #include <tuple>
 
-bool SpinQuantumNumberCounter::pairCompare(const std::tuple<int, int, int> &pair1, const std::tuple<int, int, int> &pair2) {
-    return std::get<0>(pair1)>std::get<0>(pair2);
+bool SpinQuantumNumberCounter::pairCompare(const std::tuple<int, int, int> &tuple1, const std::tuple<int, int, int> &tuple2) {
+    return std::get<0>(tuple1)>std::get<0>(tuple2);
 }
 
 SpinQuantumNumberCounter::SpinQuantumNumberCounter() {
@@ -36,9 +36,11 @@ const int & SpinQuantumNumberCounter::addNumber(const int &newNumber) {
     return newNumber;
 }
 
-const std::vector<std::tuple<int, int,int> > &SpinQuantumNumberCounter::sort() {
+/*
+ * function sorting SpinQuantumNumbers: big abs(Ms) to small abs(Ms)
+ */
+void SpinQuantumNumberCounter::sortSpinQuantumNumbers() {
     std::sort(SpinQuantumNumbers.begin(),SpinQuantumNumbers.end(),SpinQuantumNumberCounter::pairCompare);
-    return SpinQuantumNumbers;
 }
 
 const std::vector<std::tuple<int, int, int> > &SpinQuantumNumberCounter::getSpinQuantumNumbers() const{
@@ -55,7 +57,7 @@ int SpinQuantumNumberCounter::getCountOfSpinQuantumNumber(int SpinQuantumNumber)
 
 
 void SpinQuantumNumberCounter::printStatsSpinQuantumNumber(){
-    this->sort();
+    this->sortSpinQuantumNumbers();
     std::cout << "Statistics Spin Quantum Number\n"
             "SpinQuantumNumber Count" << std::endl;
     for(std::vector<std::tuple<int,int,int> >::iterator i=SpinQuantumNumbers.begin();i!=SpinQuantumNumbers.end();i++){
@@ -64,25 +66,31 @@ void SpinQuantumNumberCounter::printStatsSpinQuantumNumber(){
     }
 }
 
+/*
+ * The count of Mutplicity S is given by adding the values for Ms=-S to +S.
+ * In theory for a given S value all Ms values -S to S would occur in same amount, leading to a symmetric distribution over the Ms values
+ * Practically, one can see a skewed distribution which is due to interpretation of Multiplicity as Ms value in amolqc.
+ * This is why the counts of a particular Ms value belonging to a specific S value is evaluated using linear interpolation.
+ */
 const std::vector<std::tuple<int,int,double> > &SpinQuantumNumberCounter::getMultiplicities() {
     if(!SpinQuantumNumbers.size())return Multiplicities;
-    this->sort();
-    for(std::vector<std::tuple<int,int,int> >::iterator i=SpinQuantumNumbers.begin();i!=SpinQuantumNumbers.end();i++){
+    this->sortSpinQuantumNumbers();                     //sorting of SpinQuantumNumbers is crucial for following algorithm
+    for(std::vector<std::tuple<int,int,int> >::iterator i=SpinQuantumNumbers.begin();i!=SpinQuantumNumbers.end();i++){          //i is iterating over the S values
         Multiplicities.emplace_back(std::get<0>(*i),std::get<1>(*i)+std::get<2>(*i),0);
-        int &Msi=std::get<0>(*i);
-        int &countpi=std::get<1>(*i);
-        int &countni=std::get<2>(*i);
-        for(std::vector<std::tuple<int,int,int> >::iterator j=i+1;j!=SpinQuantumNumbers.end();j++){
-            int &Msj=std::get<0>(*j);
-            int &countpj=std::get<1>(*j);
-            int &countnj=std::get<2>(*j);
-            int pSubCounts=((Msi-Msj)*countni+(Msi+Msj)*countpi)/(2*Msi);
-            int nSubCounts=((Msi-Msj)*countpi+(Msi+Msj)*countni)/(2*Msi);
-            pSubCounts=pSubCounts>countpj?countpj:pSubCounts;
-            nSubCounts=nSubCounts>countnj?countnj:nSubCounts;
+        const int &Msi=std::get<0>(*i);
+        const int &countiPositive=std::get<1>(*i);
+        const int &countiNegative=std::get<2>(*i);
+        for(std::vector<std::tuple<int,int,int> >::iterator j=i+1;j!=SpinQuantumNumbers.end();j++){                             //j is iterationg over the Ms values with abs(Ms)<S
+            const int &Msj=std::get<0>(*j);
+            int &countjPositive=std::get<1>(*j);
+            int &countjNegative=std::get<2>(*j);
+            int pSubCounts=((Msi-Msj)*countiNegative+(Msi+Msj)*countiPositive)/(2*Msi);
+            int nSubCounts=((Msi-Msj)*countiPositive+(Msi+Msj)*countiNegative)/(2*Msi);
+            pSubCounts=pSubCounts>countjPositive?countjPositive:pSubCounts;                                                     //sometimes the count of particular Ms value is smaller than the expected value by linear interpolation.
+            nSubCounts=nSubCounts>countjNegative?countjNegative:nSubCounts;                                                     //In such situation only the real counts are taken into account
             std::get<1>(*Multiplicities.rbegin())+=pSubCounts+nSubCounts;
-            countpj-=pSubCounts;
-            countnj-=nSubCounts;
+            countjPositive-=pSubCounts;
+            countjNegative-=nSubCounts;
         }
     }
     //calculate percentages
--- src/SpinQuantumNumberCounter.h
+++ src/SpinQuantumNumberCounter.h
@@ -13,15 +13,26 @@ public:
     SpinQuantumNumberCounter();
     virtual ~SpinQuantumNumberCounter();
     const int & addNumber(const int &newNumber);
-    const std::vector<std::tuple<int,int,int> >& sort();
     const std::vector<std::tuple<int,int,int> >& getSpinQuantumNumbers() const;
     int getCountOfSpinQuantumNumber(int SpinQuantumNumber) const;
     void printStatsSpinQuantumNumber();
     void printStatsMultiplicities();
     const std::vector<std::tuple<int,int,double> > &getMultiplicities();
 private:
+    void sortSpinQuantumNumbers();
     static bool pairCompare(const std::tuple<int,int,int> &pair1, const std::tuple<int,int,int> &pair2);
+
+    /*
+     *  SpinQuantumNumbers datastructure
+     *  Each tuple represents <abs(Ms),count of positive Ms, count of negative Ms>
+     *  for Ms=0 the tuple represents <0, count of Ms=0, 0>
+     */
     std::vector<std::tuple<int,int,int> > SpinQuantumNumbers;
+
+    /*
+     *  Multiplicities datastructure
+     *  Each tuple represents <2*S,count of S,relative count of S in percent>
+     */
     std::vector<std::tuple<int,int,double> > Multiplicities;
 };
 
