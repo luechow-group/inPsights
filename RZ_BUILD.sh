3f11c919efe3bcab99bdde50e5228c5ce806a2b6
Morian Sonnet <MorianSonnet@googlemail.com>
2017-07-20 15:43:50 +0200

    ~

--- RZ_BUILD.sh
+++ RZ_BUILD.sh
@@ -7,8 +7,9 @@ mkdir -p build_RELEASE
 cd build_DEBUG
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make all
-./src/tests/LocalSpinMultiplicity_RunAllTests
-cd ../build_RELEASE
+cd src/tests/
+./LocalSpinMultiplicity_RunAllTests
+cd ../../../build_RELEASE
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make all
 cd ..
