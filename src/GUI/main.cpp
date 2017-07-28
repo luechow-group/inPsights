f67ce96aca4b8528f3bbd4ff1e9766ce281ed474
Unknown <michael.heuer@rwth-aachen.de>
2017-07-28 12:28:37 +0200

    Changes to GUI main.

--- src/GUI/main.cpp
+++ src/GUI/main.cpp
@@ -42,9 +42,6 @@
 
 
 int main(int argc, char *argv[]) {
-  //Electron3D e1(root, QVector3D(12.0f, 1.0f, 7.0f), Spin::SpinType::Alpha);
-  //Electron3D e2(root, QVector3D(9.0f, 1.2f, 7.0f), Spin::SpinType::Beta);
-
   //Qt3DRender::QObjectPicker(); // emits signals for you to handle
   //Qt3DRender::QPickingSettings* pickingSettings = new Qt3DRender::QPickingSettings();
   //pickingSettings->setPickMethod(Qt3DRender::QPickingSettings::PickMethod::BoundingVolumePicking);
@@ -52,87 +49,84 @@ int main(int argc, char *argv[]) {
   //QGuiApplication app(argc, argv);
   QApplication app(argc, argv);
 
+  /*ElectronicWaveFunction::getInstance("Ethane-em-5.wf");
+  ElectronicWaveFunction::getInstance().setFrozenElectrons({1,2,3,4,5, 10,11,12,13,14});
 
-  // H2
-  //Eigen::VectorXd xA(2 * 3);
-  //Eigen::VectorXd xB(2 * 3);
-  //xA << 0.0, 0.0, +0.70014273, 0.0, 0.00, -0.70014273;
-  //xB << 0.0, 0.0, -0.70014273, 0.0, 0.00, -0.70014273;
-
-  Eigen::VectorXd xA(18*3);
-  Eigen::VectorXd xB(18*3);
-  /*CTR*/
-//  xA <<  \
-// 0.000000, 0.000000, 1.443089,\
-// 0.000000, 0.000000,-1.443089,\
-//-1.663110, 0.960208,-2.193991,\
-// 0.000000, 1.920396, 2.193991,\
-//-1.663110,-0.960208, 2.193991,\
-// 0.000637,-0.881335,-1.752072,\
-// 0.762826, 0.441312,-1.754787,\
-// 0.762219,-0.441827, 1.754450,\
-// 0.000696, 0.003032, 0.695882,/*9*/\
-// 0.000000, 0.000000, 1.443089,\
-// 0.000000, 0.000000,-1.443089,\
-// 1.663110,-0.960208, 2.193991,\
-// 0.000000,-1.920396,-2.193991,\
-// 1.663110, 0.960208,-2.193991,\
-//-0.000261, 0.881214, 1.752389,\
-//-0.762453,-0.441670, 1.755289,\
-//-0.001111,-0.002637,-0.695660,\
-//-0.762504, 0.441871,-1.753631/*18*/;
-
-  /*max*/
+  Eigen::VectorXd xA(ElectronicWaveFunction::getInstance().getNumberOfElectrons()*3);
   xA << \
- 0.000000, 0.000000, 1.443089,\
- 0.000000, 0.000000,-1.443089,\
--1.663110, 0.960208,-2.193991,\
- 0.000000, 1.920396, 2.193991,\
--1.663110,-0.960208, 2.193991,\
--0.023346,-0.882838,-1.743612,\
- 0.752879, 0.461638,-1.743608,\
- 0.756924,-0.437014, 1.772883,\
--0.044372, 0.025617, 0.695896,\
- 0.000000, 0.000000, 1.443089,\
- 0.000000, 0.000000,-1.443089,\
- 1.663110,-0.960208, 2.193991,\
- 0.000000,-1.920396,-2.193991,\
- 1.663110, 0.960208,-2.193991,\
- 0.023348, 0.882821, 1.743604,\
--0.752862,-0.461629, 1.743600,\
- 0.044374,-0.025617,-0.695900,\
--0.756913, 0.437007,-1.772876;
+ 0.000000, 0.000000, 1.443184,\
+ 0.000000, 0.000000,-1.443184,\
+-1.662146,-0.959641, 2.192989,\
+-1.662146, 0.959641,-2.192989,\
+ 0.000000,-1.919300,-2.192989,\
+-0.024099, 0.773535, 1.718336,\
+ 0.657845,-0.407636, 1.718335,\
+ 0.658424, 0.380140,-1.752435,\
+-0.034848,-0.020119,-0.660100,\
+ 0.000000, 0.000000, 1.443184,\
+ 0.000000, 0.000000,-1.443184,\
+ 1.662146,-0.959641, 2.192989,\
+ 1.662146, 0.959641,-2.192989,\
+ 0.000000, 1.919300, 2.192989,\
+-0.657846, 0.407635,-1.718336,\
+ 0.024100,-0.773537,-1.718336,\
+-0.658423,-0.380140, 1.752435,\
+ 0.034847, 0.020120, 0.660096;
 
   Eigen::Vector3d el9= xA.segment((9-1)*3,3);
-  Eigen::Vector3d el17= xA.segment((17-1)*3,3);
+  Eigen::Vector3d el18= xA.segment((18-1)*3,3);
+  Eigen::VectorXd xB(ElectronicWaveFunction::getInstance().getNumberOfElectrons()*3);
+  xB = xA;
+  xB.segment((9-1)*3,3) = el18;
+  xB.segment((18-1)*3,3) = el9;*/
+
+
+  ElectronicWaveFunction::getInstance("Ethylene-em-5.wf");
+  ElectronicWaveFunction::getInstance().setFrozenElectrons({1,2,3,4, 9,10,11,12});
+
+  Eigen::VectorXd xA(ElectronicWaveFunction::getInstance().getNumberOfElectrons()*3);
+  xA <<\
+ 0.000000, 0.000000, 1.251320,\
+ 0.000000, 0.000000,-1.251320,\
+ 0.000000, 1.738964,-2.326820,\
+ 0.000000, 1.738964, 2.326820,\
+ 0.065175,-0.700893,-1.734994,\
+-0.065176,-0.700880, 1.734990,\
+-0.560405, 0.001751,-0.796635,\
+ 0.560397, 0.001751, 0.796612,\
+ 0.000000, 0.000000, 1.251320,\
+ 0.000000, 0.000000,-1.251320,\
+ 0.000000,-1.738964, 2.326820,\
+ 0.000000,-1.738964,-2.326820,\
+ -0.065175, 0.700891,-1.734992,\
+ 0.065173, 0.700883, 1.734990,\
+ 0.560406,-0.001752,-0.796632,\
+-0.560402,-0.001747, 0.796618;
+
+  Eigen::Vector3d el7= xA.segment((7-1)*3,3);
+  Eigen::Vector3d el8= xA.segment((8-1)*3,3);
   Eigen::Vector3d el15= xA.segment((15-1)*3,3);
   Eigen::Vector3d el16= xA.segment((16-1)*3,3);
-  //Eigen::Vector3d el6= xA.segment((6-1)*3,3);
-  //Eigen::Vector3d el7= xA.segment((7-1)*3,3);
-  //Eigen::Vector3d el18= xA.segment((18-1)*3,3);
-
+  Eigen::VectorXd xB(ElectronicWaveFunction::getInstance().getNumberOfElectrons()*3);
 
   xB = xA;
-  xB.segment((9-1)*3,3) = el17;
-  xB.segment((17-1)*3,3) = el9;
-  //xB.segment((15-1)*3,3) = el16;
-  //xB.segment((16-1)*3,3) = el15;
-  //xB.segment((6-1)*3,3) = el7;
-  //xB.segment((7-1)*3,3) = el18;
-  //xB.segment((18-1)*3,3) = el6;
+  xB.segment((7-1)*3,3)  = el15;
+  xB.segment((8-1)*3,3)  = el16;
+  xB.segment((15-1)*3,3) = el7;
+  xB.segment((16-1)*3,3) = el8;
 
   //Eigen::VectorXd bend(numberOfStates);
   //bend << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
   //x-bend
   //initialCoordinates.row((9-1)*3+0) += bend; //+x bend
   //initialCoordinates.row((17-1)*3+0) -= bend;//-x bend
-
   //y-bend
   //initialCoordinates.row((9-1)*3+1) += bend; //+y bend
   //initialCoordinates.row((17-1)*3+1) -= bend;//-y bend
 
 
-  ElectronicWaveFunction::getInstance("t.wf").evaluate(xA);
+
+
   std::cout << "phi " << ElectronicWaveFunction::getInstance().getDeterminantProbabilityAmplitude() << std::endl;
   std::cout << "U " << ElectronicWaveFunction::getInstance().getJastrowFactor() << std::endl;
   std::cout << "phi*exp(U) " << ElectronicWaveFunction::getInstance().getProbabilityAmplitude() << std::endl;
@@ -143,8 +137,8 @@ int main(int argc, char *argv[]) {
   std::cout << ElectronicWaveFunction::getInstance().getDeterminantProbabilityAmplitude() << std::endl;
 
 
-  unsigned numberOfStates = 9;
-  Eigen::MatrixXd initialCoordinates(18 * 3, numberOfStates);
+  unsigned numberOfStates = 11;
+  Eigen::MatrixXd initialCoordinates(ElectronicWaveFunction::getInstance().getNumberOfElectrons()*3, numberOfStates);
   Eigen::VectorXd delta = xB - xA;
   for (int i = 0; i < numberOfStates; ++i) {
     double rel = double(i) / double(numberOfStates - 1);
@@ -163,10 +157,10 @@ int main(int argc, char *argv[]) {
   BSplines::StationaryPointFinder stationaryPointFinder(bspline);
   std::vector<double> result = stationaryPointFinder.getMaxima(0);
 
-  Eigen::VectorXd tsGuessGeom = bspline.evaluate(result[0]).tail(18*3);
+  Eigen::VectorXd tsGuessGeom = bspline.evaluate(result[0]).tail(ElectronicWaveFunction::getInstance().getNumberOfElectrons()*3);
   std::cout << "u=" << result[0] << "\n" << tsGuessGeom.transpose() << std::endl;
 
-
+/*
   ElectronicWaveFunctionProblem f("t.wf");
   Eigen::MatrixXd hess(18*3,18*3);
   f.hessian(tsGuessGeom,hess);
@@ -191,19 +185,20 @@ int main(int argc, char *argv[]) {
   eigenSolver = Eigen::EigenSolver<Eigen::MatrixXd>(hess,false);
   eigenvalues = eigenSolver.eigenvalues();
   std::cout << eigenvalues << std::endl;
-
-
-
+*/
 
   Qt3DCore::QEntity *root = new Qt3DCore::QEntity();
 
   //Draw tsguess
-  for (int j = 0; j < tsGuessGeom.rows()/3 ; ++j) {
+  for (int j = 0; j < ElectronicWaveFunction::getInstance().getNumberOfElectrons() ; ++j) {
     Eigen::Vector3d vec3d = tsGuessGeom.segment(j * 3, 3);
     QVector3D qVector3D(vec3d(0), vec3d(1), vec3d(2));
 
-    if (j < (xA.rows() / 3) / 2) Electron3D(root, qVector3D, Spin::Alpha);
-    else Electron3D(root, qVector3D, Spin::Beta);
+    Electron3D* e = new Electron3D(root, qVector3D, Spin::None);
+    e->setRadius(0.025f);
+
+    //if (j < (xA.rows() / 3) / 2) Electron3D(root, qVector3D, Spin::Alpha);
+    //else Electron3D(root, qVector3D, Spin::Beta);
   }
 
   // draw molecular geometry
@@ -213,7 +208,7 @@ int main(int argc, char *argv[]) {
   MolecularGeometry3D molecularGeometry3D (root, waveFunctionParser.getAtomCollection());
 
   // draw electrons
-  for (int j = 0; j < xA.rows()/3 ; ++j) {
+  for (int j = 0; j < ElectronicWaveFunction::getInstance().getNumberOfElectrons() ; ++j) {
     Eigen::Vector3d vec3d = xA.segment(j*3,3);
     QVector3D qVector3D (vec3d(0),vec3d(1),vec3d(2));
 
@@ -231,11 +226,12 @@ int main(int argc, char *argv[]) {
         auto *textMesh = new Qt3DExtras::QExtrudedTextMesh();
 
         auto *textTransform = new Qt3DCore::QTransform();
-        QFont font(QString("Arial"), 10, 0, false);
+        QFont font(QString("Arial"), 12
+                , 0, false);
       QVector3D shift;
 
-      if(e->getSpinType() == Spin::Alpha) shift = QVector3D(0.0f,0.05f,0.05f);
-      else shift = QVector3D(-0.0f,-0.05f,-0.05f);
+      if(e->getSpinType() == Spin::Alpha) shift = QVector3D(0.0f,0.07f,0.07f);
+      else shift = QVector3D(-0.0f,-0.07f,-0.07f);
 
         textTransform->setTranslation(qVector3D+shift);
         textTransform->setRotationY(90);
@@ -252,10 +248,10 @@ int main(int argc, char *argv[]) {
   }
 
   BSplines::ArcLengthParametrizedBSpline bs = stringMethod.getArcLengthParametrizedBSpline();
-  StringMethodCoordinatesPlotter bSplinePlotter(root,bs, 50, 0.005f);
+  StringMethodCoordinatesPlotter bSplinePlotter(root,bs, 100, 0.01f);
 
   Qt3DExtras::Qt3DWindow *view = new Qt3DExtras::Qt3DWindow();
-  view->defaultFrameGraph()->setClearColor(Qt::gray);
+  view->defaultFrameGraph()->setClearColor(Qt::white);
 
   QWidget *moleculeView = QWidget::createWindowContainer(view);
 
@@ -263,10 +259,11 @@ int main(int argc, char *argv[]) {
   // camera
   Qt3DRender::QCamera *camera = view->camera();
   camera->lens()->setPerspectiveProjection(45.0f, 16.0f / 9.0f, 0.1f, 100.0f);
-  camera->setPosition(QVector3D(6.0, 0.0, 0.0));
+  camera->setPosition(QVector3D(4.25, 4.25, 0.0)); // ethane
   camera->setViewCenter(QVector3D(0, 0, 0));
   //camera->setFarPlane(1000);
   //camera->setNearPlane(0.1);
+  //camera->tilt(-5.0f);
 
   // manipulator
   //Qt3DExtras::QFirstPersonCameraController* manipulator = new Qt3DExtras::QFirstPersonCameraController(scene);
@@ -274,6 +271,7 @@ int main(int argc, char *argv[]) {
   manipulator->setLinearSpeed(50.f);
   manipulator->setLookSpeed(180.f);
   manipulator->setCamera(camera);
+
 
   view->setRootEntity(scene);
 
@@ -324,7 +322,7 @@ int main(int argc, char *argv[]) {
   widget->setWindowTitle(QStringLiteral("String Method Result"));
 
   widget->show();
-  widget->resize(800, 800);
+  widget->resize(1920, 1080);
 
   return app.exec();
 
