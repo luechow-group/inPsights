bf9055f1bf8b11e76c700e7cf9e7d0d1a91521a8
Leonard Reuter <reuter@pc.rwth-aachen.de>
2018-03-09 10:53:07 +0100

    Added ToString to BaseLib to convert vector3d and doubles.

---
+++ src/BaseLib/include/ToString.h
@@ -0,0 +1,21 @@
+//
+// Created by Leonard Reuter on 09.03.18.
+//
+
+#ifndef AMOLQCGUI_TOSTRING_H
+#define AMOLQCGUI_TOSTRING_H
+
+#include <string>
+#include <Eigen/Core>
+
+namespace ToString {
+    std::string double2string(double a,
+                              unsigned decimalPlaces = 5,
+                              unsigned leadingSpaces = 1);
+
+    std::string vector3d2string(const Eigen::Vector3d &vector,
+                                unsigned decimalPlaces = 5,
+                                unsigned leadingSpaces = 1);
+}
+
+#endif //AMOLQCGUI_TOSTRING_H
---
+++ src/BaseLib/source/ToString.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Leonard Reuter on 09.03.18.
+//
+
+#include <iomanip>
+#include "ToString.h"
+
+
+
+std::string ToString::vector3d2string(const Eigen::Vector3d &vector,
+                            const unsigned decimalPlaces, const unsigned leadingSpaces) {
+    std::ostringstream sstream;
+    for (int i = 0; i < vector.size(); i++){
+        sstream << " " << double2string(vector(i),decimalPlaces,leadingSpaces);
+    }
+    return sstream.str();
+}
+
+std::string ToString::double2string(double a, const unsigned decimalPlaces, const unsigned leadingSpaces) {
+    std::ostringstream sstream;
+    if (a >= 0){
+        sstream << " ";
+    }
+    for (int i = 1; i <= leadingSpaces; i++){
+        if (abs(a) < pow(10.0,i)){
+            sstream << " ";
+        }
+    }
+    sstream << std::fixed << std::setprecision(decimalPlaces) << a;
+    return sstream.str();
+}
