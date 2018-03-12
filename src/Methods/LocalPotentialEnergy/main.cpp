f8c057afcbeb5e6b4482ec0b910b81dbca9d384c
Leonard Reuter <reuter@pc.rwth-aachen.de>
2018-03-12 15:06:19 +0100

    Cleaned main.

--- src/Methods/LocalPotentialEnergy/main.cpp
+++ src/Methods/LocalPotentialEnergy/main.cpp
@@ -3,15 +3,10 @@
 //
 
 #include <iostream>
-#include "Particle.h"
 
 int main(){
-    std::cout << "hello world" << std::endl;
 
-    auto position = Eigen::Vector3d(0.1,0.2,0.35);
-    auto particle = Particle(position);
 
-    std::cout << particle << std::endl;
 
     return 0;
 }
