ebd1bd24cbdd324232a9525ee5d5b1c92894f3c5
Leonard Reuter <reuter@pc.rwth-aachen.de>
2018-03-12 23:31:15 +0100

    Added LagrangeProblem.h and test.
    Fix

---
+++ src/Methods/LocalPotentialEnergy/include/LagrangeProblem.h
@@ -0,0 +1,66 @@
+//
+// Created by leonard on 22.02.18.
+//
+
+#ifndef AMOLQCGUI_LAGRANGEPROBLEM_H
+#define AMOLQCGUI_LAGRANGEPROBLEM_H
+
+#include <problem.h>
+#include <iomanip>
+
+template<class ProblemTypeProblem, class ProblemTypeConstraint>
+class LagrangeProblem : public cppoptlib::Problem<double,Eigen::Dynamic>{
+public:
+    LagrangeProblem(ProblemTypeProblem &problem, ProblemTypeConstraint &constraint, double equality = 0)
+            : equality_(equality),
+              problem_(problem),
+              constraint_(constraint)
+            {};
+
+    double value(const Eigen::VectorXd &x) override{
+        auto x0 = x.block(0,0,x.size()-1,1); // vector for problem and condition
+        double lambda = x[x.size()-1];
+
+        return problem_.value(x0) + lambda*(constraint_.value(x0) - equality_);
+    };
+
+    void gradient(const Eigen::VectorXd &x, Eigen::VectorXd &grad) override{
+        Eigen::VectorXd x0 = x.block(0,0,x.size()-1,1);
+
+        double lambda = x[x.size()-1];
+
+        // calculating gradients of x0 elements
+        Eigen::VectorXd gradP = grad.block(0,0,x.size()-1,1); // gradient of problem
+        Eigen::VectorXd gradC = gradP; // gradient of condition
+
+        constraint_.gradient(x0, gradC);
+        problem_.gradient(x0, gradP);
+
+
+        auto grad0 = gradP + lambda * gradC;
+
+        // adding gradients of x0 elements to grad
+        for (int i = 0; i < grad0.size(); i++) {
+            grad(i) = grad0(i);
+        }
+
+        // adding gradient of lambda to grad
+        grad(grad.size()-1) = constraint_.value(x0) - equality_;
+    };
+
+    bool callback(const cppoptlib::Criteria<double> &state, Eigen::VectorXd &x, Eigen::VectorXd& grad) override{
+        std::cout << "(" << std::setw(2) << state.iterations << ")"
+                  << " f(x) = " << std::fixed << std::setw(8) << std::setprecision(8) << value(x)
+                  << " xDelta = " << std::setw(8) << state.xDelta
+                  << " gradInfNorm = " << std::setw(8) << state.gradNorm
+                  << "   " << x.transpose()
+                  << std::endl;
+    };
+
+private:
+    ProblemTypeConstraint constraint_;
+    ProblemTypeProblem problem_;
+    double equality_;
+};
+
+#endif //AMOLQCGUI_LAGRANGEPROBLEM_H
---
+++ src/Methods/LocalPotentialEnergy/tests/ALangrangeProblemTest.cpp
@@ -0,0 +1,106 @@
+//
+// Created by Leonard Reuter on 12.03.18.
+//
+
+#include <gmock/gmock.h>
+#include <Eigen/Core>
+#include "LagrangeProblem.h"
+#include "solver/gradientdescentsolver.h"
+
+using namespace testing;
+using namespace Eigen;
+
+class ALagrangeProblemTest : public Test {public:
+    void SetUp() override {};
+};
+
+class testProblem: public cppoptlib::Problem<double,Eigen::Dynamic>{
+public:
+    double value(const Eigen::VectorXd & x) override {
+        double value = 0.0;
+        for (int i = 0; i < x.size(); i++){
+            value += x[i];
+        }
+    };
+
+    void gradient(const Eigen::VectorXd & x, Eigen::VectorXd &grad) override {
+        for (int i = 0; i < x.size(); i++){
+            grad[i] = 1;
+        }
+    };
+};
+
+class testConstraint : public cppoptlib::Problem<double,Eigen::Dynamic>{
+public:
+    double value(const Eigen::VectorXd & x) override {
+        double value = 0.0;
+        for (int i = 0; i < x.size(); i++){
+            value += x[i]*x[i];
+        }
+    };
+
+    void gradient(const Eigen::VectorXd & x, Eigen::VectorXd &grad) override {
+        for (int i = 0; i < x.size(); i++){
+            grad[i] = 2*x[i];
+        }
+    };
+};
+
+TEST(ALagrangeProblemTest, Construction) {
+    testProblem problem;
+    testConstraint constraint;
+    LagrangeProblem<testProblem,testConstraint> lagrangeProblem(problem,constraint,1);
+}
+
+//took test functions from wikipedia
+TEST(ALagrangeProblemTest, Value) {
+    testProblem problem;
+    testConstraint constraint;
+    LagrangeProblem<testProblem,testConstraint> lagrangeProblem(problem,constraint,1);
+
+    Eigen::VectorXd y(3);
+    y << Vector3d(-2,-2,2);
+
+    ASSERT_DOUBLE_EQ(lagrangeProblem.value(y),10);
+}
+
+TEST(ALagrangeProblemTest, Gradient) {
+    testProblem problem;
+    testConstraint constraint;
+    LagrangeProblem<testProblem,testConstraint> lagrangeProblem(problem,constraint,1);
+
+    Eigen::VectorXd y(3);
+    y << Vector3d(-2,-2,2);
+
+    Eigen::VectorXd gradient = y;
+    lagrangeProblem.gradient(y,gradient);
+
+    Eigen::VectorXd reference(3);
+    reference << Vector3d(-7,-7,7);
+    ASSERT_EQ(gradient, reference);
+}
+
+TEST(ALagrangeProblemTest, Optimization) {
+    testProblem problem;
+    testConstraint constraint;
+    LagrangeProblem<testProblem,testConstraint> lagrangeProblem(problem,constraint,1);
+
+    Eigen::VectorXd y(3);
+    y << Vector3d(-2,-2,2);
+
+    Eigen::VectorXd gradient = y;
+    lagrangeProblem.gradient(y,gradient);
+
+    cppoptlib::Criteria<double> crit = cppoptlib::Criteria<double>::defaults();
+
+    cppoptlib::GradientDescentSolver<LagrangeProblem<testProblem,testConstraint>> solver;
+    solver.setDebug(cppoptlib::DebugLevel::High);
+    solver.setStopCriteria(crit);
+
+    solver.minimize(lagrangeProblem, y);
+
+    Eigen::VectorXd reference(3);
+    reference << Vector3d(-0.70713277,-0.70713277,0.70713277);
+
+    ASSERT_GT(1e-8,(y-reference).norm());
+}
