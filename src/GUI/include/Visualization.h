b3f67134a489df94f0034c13ec61f4a563fdf648
Leonard Reuter <reuter@pc.rwth-aachen.de>
2018-03-15 14:34:27 +0100

    Added Visualization.cpp to GUI.

--- src/GUI/include/Visualization.h
+++ src/GUI/include/Visualization.h
@@ -23,50 +23,7 @@ namespace Visualization{
     int visualizeOptPath(int &argc, char **argv,
                          const AtomsVector &atoms,
                          const ElectronsVectorCollection &optimizationPath,
-                         const unsigned long &nwanted = 300) {
-        QApplication app(argc, argv);
-        setlocale(LC_NUMERIC,"C");
-
-
-        ElectronsVectorCollection shortenedPath(optimizationPath[0]);
-
-        double optPathLength = 0.0;
-        Eigen::VectorXd pathLengthVector =
-                Eigen::VectorXd::Zero(optimizationPath.numberOfEntities());
-
-        for (unsigned long i = 1; i < optimizationPath.numberOfEntities(); i++){
-            optPathLength += optimizationPath.norm(i,i - 1);
-            pathLengthVector[i] = optPathLength;
-        }
-
-        double stepLength = optPathLength / nwanted;
-        ElectronsVector elecVector;
-        unsigned long index = 0;
-        for (unsigned long i = 0; i <= nwanted; i++) {
-            index = 0;
-            for (unsigned long j = 1; j < optimizationPath.numberOfEntities(); j++){
-                if (fabs(pathLengthVector[j] - i * stepLength) <
-                    fabs(pathLengthVector[index] - i * stepLength)){
-                    index = j;
-                }
-            }
-            shortenedPath.append(optimizationPath[index]);
-        }
-
-        // Visualization
-        MoleculeWidget moleculeWidget;
-        Qt3DCore::QEntity *root = moleculeWidget.createMoleculeWidget();
-
-        AtomsVector3D(root, atoms);
-
-        // Plot the starting point
-        ElectronsVector3D(root, optimizationPath[-1], false);
-
-        // Plot the optimization path
-        ParticlesVectorPath3D(root, shortenedPath);
-
-        return app.exec();
-    }
+                         const unsigned long &nwanted = 300);
 }
 
 #endif //AMOLQCPP_VISUALIZATION_H
