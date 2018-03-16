d6d61768d5f81c4c45d5131705c1467c45b278f7
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-03-16 09:19:43 +0100

    Added expander class and test.

---
+++ src/Methods/StructuralSimilarity/include/SphericalHarmonicsRadialBasisExpander.h
@@ -0,0 +1,101 @@
+//
+// Created by Michael Heuer on 15.03.18.
+//
+
+#ifndef AMOLQCPP_SPHERICALHARMONICSRADIALBASISEXPANDER_H
+#define AMOLQCPP_SPHERICALHARMONICSRADIALBASISEXPANDER_H
+
+#include <sh/spherical_harmonics.h>
+#include <LebedevSphericalIntegration/SpatialFunction.h>
+#include <SphericalIntegrator.h>
+#include "RadialBasis.h"
+
+#include <vector>
+
+class ExpansionSettings{
+public:
+    unsigned lmax;
+    unsigned nmax;
+    double rCutoff;
+};
+
+/*std::vector<std::vector<std::vector<double >>> coeffs = {
+        {{0}, {-1,0,1}, {-2,-1,0,1,2}}, // n = 1
+        {{0}, {-1,0,1}, {-2,-1,0,1,2}} // n = 2
+        //...
+
+};*/
+
+
+class SphericalHarmonicsRadialBasisExpander : public SpatialFunction{
+public:
+    SphericalHarmonicsRadialBasisExpander(unsigned lmax, unsigned nmax, double rCutoff)
+            : fPtr_(nullptr),
+              radialBasis_(nmax, rCutoff),
+              sphericalIntegrator_(SphericalIntegratorSettings::expansion(lmax,nmax,rCutoff)),
+              lmax_(lmax),nmax_(nmax),l_(0),n_(0),m_(0),rCutoff_(rCutoff) {};
+
+
+    std::vector<std::vector<std::vector<double >>> coefficients(SpatialFunction & f){
+        std::vector<std::vector<std::vector<double >>> coefficients;
+        coefficients = {};
+        for (unsigned n = 1; n <= nmax_; ++n) {
+            //coefficients[n-1].emplace_back({});
+            for (unsigned l = 0; l < lmax_; ++l) {
+                //coefficients[n-1][l].emplace_back({});
+                for (int m = -lmax_; m < +lmax_; ++m) {
+                    coefficients[n-1][l].emplace_back(coefficient(f,n,l,m));
+                }
+            }
+        }
+        return coefficients;
+    };
+
+    // f might be an AtomicNeighborhoodDensityFunction
+    double coefficient(SpatialFunction & f, unsigned n, unsigned l, int m){
+        set_n(n); set_l(l); set_m(m);
+
+        setFunctionPtr(f);
+        return sphericalIntegrator_.integrate(*this, rCutoff_);
+    };
+
+private:
+
+    // This method is handed to the spherical integrator via *this
+    double value(const Eigen::Vector3d & rvec){
+        return radialBasis_(rvec.norm(),n_)*sh::EvalSH(l_,m_,rvec)* fPtr_->value(rvec);
+    };
+
+    void setFunctionPtr(const SpatialFunction &f){
+        fPtr_ = const_cast<SpatialFunction*>(&f);
+    };
+
+    void set_n(unsigned n){
+        assert(n > 0 && "n must be greater than zero.");
+        assert(n <= nmax_ && "n must be smaller than or equal to nmax.");
+        n_ = n;
+    };
+    void set_l(unsigned l){
+        assert(l >= 0 && "l must be greater than or equal to zero.");
+        assert(n <= lmax_ && "l must be smaller than or equal to lmax.");
+        l_ = l;
+    };
+    void set_m(int m){
+        assert(m >= -lmax_ && "m must be greater than or equal to -lmax.");
+        assert(n <= +lmax_ && "m must be smaller than or equal to lmax.");
+        m_ = m;
+    };
+
+
+    SpatialFunction* fPtr_; // needed to access specialized f_->value
+    RadialBasis radialBasis_;
+    SphericalIntegrator sphericalIntegrator_;
+
+
+    unsigned lmax_, nmax_, l_, n_;
+    int m_;
+    double rCutoff_;
+
+};
+
+#endif //AMOLQCPP_SPHERICALHARMONICSRADIALBASISEXPANDER_H
---
+++ src/Methods/StructuralSimilarity/tests/ASphericalHarmonicsRadialBasisExpanderTest.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Michael Heuer on 16.03.18.
+//
+
+#include <gtest/gtest.h>
+#include <LebedevSphericalIntegration/TestFunctions.h>
+#include "SphericalHarmonicsRadialBasisExpander.h"
+
+
+using namespace testing;
+
+class ASphericalHarmonicsRadialBasisExpanderTest : public ::testing::Test {};
+
+
+
+TEST_F(ASphericalExpanderTest, CoefficientsVector) {
+
+    UnitSphere f;
+
+    unsigned nmax = 4;
+    unsigned lmax = 2;
+    double rCutoff = 1.0;
+
+    SphericalHarmonicsRadialBasisExpander expander(nmax,lmax,rCutoff);
+
+    auto coeffs = expander.coefficients(f);
+
+    for (unsigned n = 1; n <= nmax; ++n) {
+        for (unsigned l = 0; l < lmax; ++l) {
+            std::cout << "\n";
+            for (int m = -lmax; m < +lmax; ++m) {
+                std::cout << coeffs[n-1][l][m+lmax] << "";
+            }
+            std::endl;
+        }
+    }
+
+
+}
