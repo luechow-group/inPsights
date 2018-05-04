4c64c524ec8a6e09104f6fb0b8c72baa2f869a77
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-05-04 18:22:54 +0200

    Added SoapExpansion and Spectrum class.

---
+++ src/Methods/StructuralSimilarity/include/SoapExpansion.h
@@ -0,0 +1,64 @@
+//
+// Created by Michael Heuer on 20.04.18.
+//
+
+#ifndef AMOLQCPP_SOAPEXPANSION_H
+#define AMOLQCPP_SOAPEXPANSION_H
+
+#include "RadialGaussianBasis.h"
+#include "AngularBasis.h"
+#include "CoefficientsVector.h"
+#include <Particle.h>
+
+//TODO find better name e.g. NeighborBasisExpander
+template <typename Type>
+class SoapExpansion{
+public:
+    explicit SoapExpansion(const ExpansionSettings& settings = ExpansionSettings::defaults())
+            : s_(settings),
+              radialGaussianBasis_(s_),
+              angularBasis_(s_.angular)
+    {}
+
+    CoefficientsVector<Type> expandParticle(const Particle<Type> &center,
+                                            const Particle<Type> &neighbor,
+                                            double neighborSigma) const {
+        double theta,phi;
+
+        Eigen::Vector3d centerToNeighborVector = (neighbor.position()-center.position());
+        double centerToNeighborDistance = centerToNeighborVector.norm();
+
+        if(centerToNeighborDistance > 0.) {
+            BoostSphericalHarmonics::ToSphericalCoords(centerToNeighborVector.normalized(), theta, phi);
+            if (phi < 0.) phi += 2 * M_PI;
+        } else { // center and neighbor positions are identical
+            theta = 0.;
+            phi = 0.;
+        }
+
+        CoefficientsVector<Type> coefficientsVector(s_);
+
+        for (unsigned n = 1; n <= s_.radial.nmax; ++n) {
+            for (unsigned l = 0; l <= s_.angular.lmax+1; ++l) {
+                for (int m = -l; m <= l; ++m) {
+                    auto coefficient = radialGaussianBasis_.computeCoefficient(n, l, centerToNeighborDistance, neighborSigma)
+                                       * angularBasis_.computeCoefficient(l, m, theta, phi);
+
+                    coefficientsVector.storeCoefficient(0, n, l, m, coefficient);
+                }
+            }
+        }
+        return coefficientsVector;
+    }
+
+    const ExpansionSettings& getSettings(){
+        return s_;
+    }
+
+private:
+    ExpansionSettings s_;
+    RadialGaussianBasis radialGaussianBasis_;
+    AngularBasis angularBasis_;
+};
+
+#endif //AMOLQCPP_SOAPEXPANSION_H
---
+++ src/Methods/StructuralSimilarity/include/Spectrum.h
@@ -0,0 +1,69 @@
+//
+// Created by Michael Heuer on 18.04.18.
+//
+
+#ifndef AMOLQCPP_SPECTRUM_H
+#define AMOLQCPP_SPECTRUM_H
+
+#include "Cutoff.h"
+#include "SoapExpansion.h"
+#include <ParticlesVector.h>
+#include <vector>
+
+template <typename Type>
+class Spectrum{
+public:
+
+    explicit Spectrum(const ParticlesVector<Type>& particlesVector,
+                      const ExpansionSettings& settings = ExpansionSettings::defaults())
+            : particlesVector_(particlesVector),
+              coefficientsVector_(unsigned(particlesVector_.numberOfEntities()), settings),
+              soapExpansion_(settings)
+    {}
+
+    void compute(){
+        for (unsigned i = 0; i < particlesVector_.numberOfEntities(); ++i) {
+            expandParticlesInNeighborhood(i);
+        }
+    }
+
+    void expandParticlesInNeighborhood(unsigned centerParticleId){
+        assert(centerParticleId >= 0
+               && "The center particle ID must be greater or equal to zero.");
+        assert(centerParticleId < particlesVector_.numberOfEntities()
+               && "The center particle ID must be smaller than the total number of particles.");
+
+        for (unsigned j = 0; j < particlesVector_.numberOfEntities(); ++j) {
+
+            /*//TODO arbitrary? change this */ double neighborSigma = soapExpansion_.getSettings().radial.sigmaAtom;
+
+            double centerToNeighborDistance =Cutoff::distance(particlesVector_[j].position(),
+                                                              particlesVector_[centerParticleId].position());
+
+            // skip this iteration if particle i is outside the cutoff radius
+            if (!cutoffFunction_.withinCutoffRadiusQ(centerToNeighborDistance)){
+                continue;
+            } else {
+                double weight = 1;
+                double weightScale = cutoffFunction_.getWeight(centerToNeighborDistance);
+
+                if (j == centerParticleId) weight *= cutoffFunction_.getCenterWeight();
+
+                auto atomicCoefficientsVector = soapExpansion_.expandParticle(particlesVector_[centerParticleId],
+                                                                              particlesVector_[j], neighborSigma);
+                atomicCoefficientsVector *= weight*weightScale;
+
+                coefficientsVector_.storeParticleExpansion(j, atomicCoefficientsVector);
+            }
+        }
+    }
+
+
+private:
+    ParticlesVector<Type> particlesVector_;
+    SoapExpansion<Type> soapExpansion_;
+    CoefficientsVector<Type> coefficientsVector_;
+    Cutoff cutoffFunction_;
+};
+
+#endif //AMOLQCPP_SPECTRUM_H
