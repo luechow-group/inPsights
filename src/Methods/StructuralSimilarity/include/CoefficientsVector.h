6c1294e02b049f1c0d70afe7198b5be9f1521010
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-05-04 13:52:24 +0200

    Refactoring and templatisation of CoefficientsVector.

--- src/Methods/StructuralSimilarity/include/CoefficientsVector.h
+++ src/Methods/StructuralSimilarity/include/CoefficientsVector.h
@@ -6,22 +6,66 @@
 #define AMOLQCPP_COEFFICIENTSVECTOR_H
 
 #include <Eigen/Core>
+#include "ExpansionSettings.h"
+#include <ParticlesVector.h>
 
-class CoefficientsVector{
+template <typename Type>
+class CoefficientsVector : AbstractVector {
 public:
 
     // Preallocates the coefficient matrix
-    CoefficientsVector(unsigned nmax, unsigned lmax);
+    explicit CoefficientsVector(ParticlesVector<Type> particleVector,
+                                const ExpansionSettings& settings = ExpansionSettings::defaults())
+            : s_(),
+              entityLength_(unsigned( s_.radial.nmax * (pow(s_.angular.lmax,2) + 2*s_.angular.lmax + 1) )),
+              coefficients_(Eigen::VectorXcd::Zero(particleVector.numberOfEntities() * entityLength_))
+    {}
 
-    std::complex<double> getCoefficient(unsigned n, unsigned l, int m) const;
+    explicit CoefficientsVector(Particle<Type> particleVector,
+                                const ExpansionSettings& settings = ExpansionSettings::defaults())
+            : s_(),
+              entityLength_(unsigned( s_.radial.nmax * (pow(s_.angular.lmax,2) + 2*s_.angular.lmax + 1) )),
+              coefficients_(Eigen::VectorXcd::Zero(entityLength_))
+    {}
 
-    void storeCoefficient(unsigned n, unsigned l, int m, const std::complex<double>& coefficient);
+    std::complex<double> getCoefficient(unsigned i, unsigned n, unsigned l, int m) const {
+    s_.checkBounds(n,l,m);
+    return coefficients_( calculateIndex(i) + (n-1) * (l*l+l+m) );
+}
 
-    void checkAngularBounds(unsigned l, int m) const;
+    void storeCoefficient(unsigned i, unsigned n, unsigned l, int m, const std::complex<double> &coefficient) {
+        s_.checkBounds(n,l,m);
+        coefficients_( calculateIndex(i) + (n-1) * (l*l+l+m) ) = coefficient;
+    }
 
-    void checkRadialBounds(unsigned n, unsigned l) const;
+    Eigen::VectorXcd asEigenVector() const {
+        return coefficients_;
+    }
+
+    long calculateIndex(long i) const override {
+        return AbstractVector::calculateIndex(i)*entityLength_;
+    }
+
+    //Atomic coefficients
+    Eigen::VectorXcd operator[](long i) const {
+        return coefficients_.segment(calculateIndex(i),entityLength_);
+    }
+
+    void storeParticleExpansion(unsigned i, const CoefficientsVector<Type> &coefficientsVector) {
+        assert(s_ == coefficientsVector.s_
+               && "The expansion settings must be identical.");
+        assert(coefficientsVector.numberOfEntities() == 1
+               && "CoeffcientsVector of the single particle must have the size 1.");
+        coefficients_.segment( calculateIndex(i), entityLength_) = coefficientsVector.asEigenVector();
+    }
+
+    const ExpansionSettings& getSettings(){
+        return s_;
+    }
+
 private:
-    unsigned nmax_, lmax_;
+    ExpansionSettings s_;
+    unsigned entityLength_;
     Eigen::VectorXcd coefficients_;
 };
 
