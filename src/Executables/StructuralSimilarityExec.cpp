746a7abc75014172f1639390536f9145328afc42
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-06-11 16:12:18 +0200

    Modified executable to allow for testing.

--- src/Executables/StructuralSimilarityExec.cpp
+++ src/Executables/StructuralSimilarityExec.cpp
@@ -3,25 +3,57 @@
 //
 #include <StructuralSimilarity.h>
 #include <AmolqcFileImport/RefFileImporter.h>
+#include <omp.h>
+#include <sstream>
 
 int main(int argc, char *argv[]) {
-    RefFileImporter importer("Ethane-max.ref");
+
+    if (argc < 4) {
+        std::cout << "Arguments missing. \n"
+                     "reference file argument must be a string ending with .ref\n"
+                     "nmax argument must be a positive integer > 0\n"
+                     "lmax argument must be a positive integer"
+                  << std::endl;
+        std::cout << "e.g. ./StructuralSimilarity.exe max.ref 10 10" << std::endl;
+        return false;
+    }
+
+    std::istringstream nmaxString(argv[2]);
+    unsigned nmax;
+    if (!(nmaxString >> nmax) || nmax <= 0) {
+        std::cerr << "Invalid number " << argv[2] << '\n';
+        return false;
+    }
+
+    std::istringstream lmaxString(argv[3]);
+    unsigned lmax;
+    if (!(lmaxString >> lmax)){
+        std::cerr << "Invalid number " << argv[3] << '\n';
+        return false;
+    }
+
+    std::cout << argv[1] << std::endl;
+    std::cout << "nmax: " << nmax << std::endl;
+    std::cout << "lmax: " << lmax << std::endl;
+
+    RefFileImporter importer(argv[1]);//"Ethane-max.ref");
     auto atomsVector = importer.getAtomsVector();
     auto numberOfSuperstructures = importer.numberOfSuperstructures();
 
     // Settings
     ExpansionSettings::defaults();
-    ExpansionSettings::Radial::nmax = 5;
-    ExpansionSettings::Angular::lmax = 5;
+    ExpansionSettings::Radial::nmax = nmax;
+    ExpansionSettings::Angular::lmax = lmax;
     ExpansionSettings::mode = ExpansionSettings::Mode::Alchemical;
     ParticleKit::create(atomsVector, importer.getMaximaStructure(1,1));
 
     std::vector<MolecularSpectrum> spectra;
     spectra.reserve(numberOfSuperstructures);
 
+    //#pragma omp parallel for shared(spectra)
     for (unsigned long i = 0; i < numberOfSuperstructures; ++i) {
         MolecularGeometry mol(atomsVector,importer.getMaximaStructure(i+1,1));
-        std::cout << mol;
+        //std::cout << mol;
         spectra.emplace(spectra.begin()+i,mol);
         std::cout << "done" << std::endl;
     }
