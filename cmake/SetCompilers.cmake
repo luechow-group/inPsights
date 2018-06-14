b2e8d662aca346de863880c3c352c8e1ca04ed87
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-06-14 16:42:46 +0200

    Allow for compiler-specific and build-type specific flags.

--- cmake/SetCompilers.cmake
+++ cmake/SetCompilers.cmake
@@ -1,4 +1,5 @@
 
+# Determine compiler from environment variables
 if(EXISTS $ENV{CC})
     set(CMAKE_C_COMPILER $ENV{CC})
     message("Using specified C Compiler         ${CMAKE_C_COMPILER}")
@@ -18,4 +19,24 @@ if(EXISTS $ENV{FC})
     message("Using specified Fortran Compiler   ${CMAKE_Fortran_COMPILER}")
 else()
     message("Using default Fortran Compiler     ${CMAKE_Fortran_COMPILER}")
+endif()
+
+
+# General settings
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+    add_definitions(-O3)
+endif()
+
+# Compiler specific optimization settings
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    # using Clang
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    # using GCC
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+    # using Intel C++
+    add_definitions(-ipo)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+    # using Visual Studio C++
+    message(" ## WARNING ##: Amolqcpp was not tested with the Microsoft Visual Studio compiler.
+    Consider switching to the Intel or GNU compiler collection ")
 endif()
