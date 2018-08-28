129d377a20894728e4f6764705684d8cc804fbb8
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-08-28 17:37:19 +0200

    Added method to calculate positional distances and added chemical hungarian tests.

--- src/BaseLib/source/Metrics.cpp
+++ src/BaseLib/source/Metrics.cpp
@@ -2,7 +2,6 @@
 // Created by Michael Heuer on 25.04.18.
 //
 
-
 #include "Metrics.h"
 
 double Metrics::distance(const Eigen::Vector3d& position1, const Eigen::Vector3d& position2){
@@ -10,9 +9,31 @@ double Metrics::distance(const Eigen::Vector3d& position1, const Eigen::Vector3d
 }
 
 
-double Metrics::distance(const PositionsVector& positionsVector1,
-                         const PositionsVector& positionsVector2){
-    assert(positionsVector1.numberOfEntities() == positionsVector2.numberOfEntities()
+double Metrics::distance(const PositionsVector& positions1,
+                         const PositionsVector& positions2){
+    assert(positions1.numberOfEntities() == positions2.numberOfEntities()
            && "Both PositionVectors must have the same size.");
-    return (positionsVector1.positionsAsEigenVector()-positionsVector2.positionsAsEigenVector()).norm();
+    return (positions1.positionsAsEigenVector()-positions2.positionsAsEigenVector()).norm();
 }
+
+Eigen::MatrixXd Metrics::positionalDistances(const PositionsVector &positions){
+    Eigen::MatrixXd d = Eigen::MatrixXd::Zero(positions.numberOfEntities(),positions.numberOfEntities());
+    for (size_t i = 0; i < d.rows(); i++)
+        for (size_t j = i + 1; j < d.cols(); j++)
+            d(i,j) = Metrics::distance(positions[i], positions[j]);
+
+    // symmetrization
+    return d.selfadjointView<Eigen::Upper>();
+};
+
+Eigen::MatrixXd Metrics::positionalDistances(const PositionsVector &positions1, const PositionsVector &positions2){
+    assert(positions1.numberOfEntities() == positions2.numberOfEntities());
+
+    Eigen::MatrixXd d = Eigen::MatrixXd::Zero(positions1.numberOfEntities(),positions1.numberOfEntities());
+
+    for (size_t i = 0; i < d.rows(); i++)
+        for (size_t j = 0; j < d.cols(); j++)
+            d(i,j) = Metrics::distance(positions1[i], positions2[j]);
+
+    return d;
+};
--- src/Methods/StructuralSimilarity/tests/AHungarianTest.cpp
+++ src/Methods/StructuralSimilarity/tests/AHungarianTest.cpp
@@ -4,6 +4,8 @@
 
 #include <gmock/gmock.h>
 #include "Hungarian.h"
+#include "Metrics.h"
+#include "TestMolecules.h"
 
 TEST(HungarianTest, OneSolution) {
     Eigen::MatrixXd input(4,4);
@@ -51,4 +53,44 @@ TEST(HungarianTest, TwoSolutions) {
     1,0,0,0;
 
     ASSERT_THAT(output,::testing::AnyOf(expectedOutputs[0],expectedOutputs[1]));
+}
+
+TEST(HungarianTest, FlippedPositions) {
+    auto p1 = TestMolecules::H2::ElectronsInCores::normal.electrons().positionsVector();;
+    auto p2 = TestMolecules::H2::ElectronsInCores::flippedSpins.electrons().positionsVector();
+
+    auto input = Metrics::positionalDistances(p1,p2);
+    ASSERT_EQ(input.rows(),2);
+    ASSERT_EQ(input.cols(),2);
+
+    Eigen::MatrixXd output(2,2);
+    Hungarian::findMatching(input,output,MATCH_MIN);
+
+    Eigen::MatrixXd expectedOutput(2,2);
+    expectedOutput << \
+    0,1,\
+    1,0;
+
+    ASSERT_EQ(output,expectedOutput);
+}
+
+
+TEST(HungarianTest, TranslatedAndFlippedPositions) {
+
+    auto p1 = TestMolecules::H2::ElectronsInCores::translated.electrons().positionsVector();;
+    auto p2 = TestMolecules::H2::ElectronsInCores::flippedSpins.electrons().positionsVector();
+
+    auto input = Metrics::positionalDistances(p1,p2);
+    ASSERT_EQ(input.rows(),2);
+    ASSERT_EQ(input.cols(),2);
+
+    Eigen::MatrixXd output(2,2);
+    Hungarian::findMatching(input,output,MATCH_MIN);
+
+    Eigen::MatrixXd expectedOutput(2,2);
+    expectedOutput << \
+    0,1,\
+    1,0;
+
+    ASSERT_EQ(output,expectedOutput);
 }
