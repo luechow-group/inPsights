c9534157d91be38c9622635f842bf7d1c7c8e7d1
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-09-07 10:18:26 +0200

    Refactoring of EnergyPartitioning methods into separate class.

--- src/Executables/EnergyPartitioning.cpp
+++ src/Executables/EnergyPartitioning.cpp
@@ -15,6 +15,98 @@ double mostDeviatingParticleDistance(const PositionsVector& ref, PositionsVector
     return Metrics::positionDistancesVector(ref,other).lpNorm<Eigen::Infinity>();
 }
 
+class AbstractSorter{
+public:
+    AbstractSorter(std::set<Reference>& references, std::vector<Sample>& samples)
+    :
+    references_(references),
+    samples_(samples),
+    console(spdlog::get("console"))
+    {}
+
+protected:
+    std::set<Reference>& references_;
+    std::vector<Sample>& samples_;
+    std::shared_ptr<spdlog::logger> console;
+};
+
+class GlobalIdentiySorter : public AbstractSorter{
+public:
+
+    GlobalIdentiySorter(std::set<Reference>& references, std::vector<Sample>& samples, double increment, double distThresh)
+    :
+    AbstractSorter(references,samples),
+    increment_(increment),
+    distThresh_(distThresh)
+    {}
+
+    GlobalIdentiySorter(std::set<Reference>& references, std::vector<Sample>& samples, double distThresh = 0.01)
+    :
+    GlobalIdentiySorter(references, samples, (*references.rbegin()).negLogSqrdProbabilityDensity_ * 1e-7, distThresh)
+    {}
+
+
+    bool doSort(){
+        if(references_.empty()) {
+            console->error("References are empty.");
+            return false;
+        }
+        else if (references_.size() == 1) {
+            console->warn("No sorting because only one reference was found.");
+            return true; // no sorting
+        }
+
+        auto lit = references_.begin();
+        auto uit = references_.begin();
+
+        while (lit != references_.end()){
+            uit = references_.upper_bound(Reference((*lit).negLogSqrdProbabilityDensity_+increment_));
+            auto it = lit;
+            it++; // start with the element next to lit
+
+            while(it != uit) subLoop(lit,it);
+
+            lit = uit;
+        }
+
+        return true;
+    }
+
+private:
+    void subLoop(std::set<Reference>::iterator& lit, std::set<Reference>::iterator& it){
+
+        //TODO CHECK MULTIPLICITY
+        auto bestMatch = HungarianHelper::spinSpecificHungarian((*it).maximum_,(*lit).maximum_);
+        auto bestMatchFlip = HungarianHelper::spinSpecificHungarian((*it).maximum_,(*lit).maximum_,true);
+
+        double dist= mostDeviatingParticleDistance(
+                (*lit).maximum_.positionsVector(),
+                (*it).maximum_.positionsVector(),bestMatch);
+
+        double distFlip = mostDeviatingParticleDistance(
+                (*lit).maximum_.positionsVector(),
+                (*it).maximum_.positionsVector(),bestMatchFlip);
+
+        if( (dist <= distThresh_) || (distFlip <= distThresh_) ){
+            // refs are identical
+
+            if(dist <= distFlip)
+                samples_[(*it).id_].sample_.permute(bestMatch);
+            else
+                samples_[(*it).id_].sample_.permute(bestMatchFlip);
+
+            (*lit).addAssociation((*it).id_);
+            (*lit).associations_.merge((*it).associations_);
+
+            it = references_.erase(it); // returns the iterator of the following element
+        } else {
+            it++;
+        }
+    }
+
+    double increment_,distThresh_;
+
+};
 
 int main(int argc, char *argv[]) {
     Logger::initialize();
@@ -28,38 +120,21 @@ int main(int argc, char *argv[]) {
 
     console->info("number of refs {}",references.size());
 
+    GlobalIdentiySorter globalIdentiySorter(references,samples);
+    globalIdentiySorter.doSort();
 
-    double increment, distThresh = 0.05;
-    if(!references.empty()) {
-        increment = (*references.rbegin()).negLogSqrdProbabilityDensity_ * 1e-4;
-        console->info("increment: {}",increment);
-        if(references.size() == 1){
-            console->warn("No sorting because only one reference was found.");
-            return true; // no sorting
+    console->flush();
+
+    for (auto a : references){
+        std::cout << a.id_ << " {";
+        for (auto b : a.associations_){
+            std::cout << b << " ";
         }
-    } else {
-        console->error("References are empty.");
-        return false;
+        std::cout << " }" << std::endl;
     }
 
-
-    // initialize
-    auto lit = references.begin();
-    auto uit = references.begin();
-
-    while (lit != references.end()){
-        console->info("lit={}",(std::distance(references.begin(),lit)));
-        uit = references.upper_bound(Reference((*lit).negLogSqrdProbabilityDensity_+increment));
-
-        auto it = lit;
-        it++;
-        console->info("before inner while  it={}", (std::distance(references.begin(),it)));
-        console->info("before inner while uit={}", (std::distance(references.begin(),uit)) );
-
-        // check all refs
-        while(it != uit) { // start with incremented iterator
-            console->info("  it={}", (std::distance(references.begin(),it)) );
-
+}
+/*console->info("  it={}", (std::distance(references.begin(),it)) );
             // check hungarian
 
             //TODO CHECK MULTIPLICITY
@@ -94,25 +169,10 @@ int main(int argc, char *argv[]) {
 
                 it = references.erase(it); // returns the iterator of the following element
                 console->info("uit={}", (std::distance(references.begin(),uit)) );
+
             } else {
                 it++;
             }
 
             console->info("end  it={}", (std::distance(references.begin(),it)) );
-            console->info("end uit={}", (std::distance(references.begin(),uit)) );
-        }
-        console->info("after inner while  it={}", (std::distance(references.begin(),it)));
-        console->info("after inner while uit={}", (std::distance(references.begin(),uit)) );
-        lit = uit;
-    }
-    console->flush();
-
-    for (auto a : references){
-        std::cout << a.id_ << " {";
-        for (auto b : a.associations_){
-            std::cout << b << " ";
-        }
-        std::cout << " }" << std::endl;
-    }
-
-}
+            console->info("end uit={}", (std::distance(references.begin(),uit)) );*/
