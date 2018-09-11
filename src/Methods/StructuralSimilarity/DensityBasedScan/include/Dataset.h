1bbb218017860d52e92ae8f918c3f6a85132f6e1
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-09-11 09:48:18 +0200

    Deleted method.

--- src/Methods/StructuralSimilarity/DensityBasedScan/include/Dataset.h
+++ src/Methods/StructuralSimilarity/DensityBasedScan/include/Dataset.h
@@ -25,91 +25,13 @@ namespace Clustering {
         Dataset()
         : rows_(0), cols_(0) {}
 
-        //void gen_cluster_data(size_t features_num, size_t elements_num);
-
-        bool load_csv(const std::string &csv_file_path) {
-            std::ifstream in(csv_file_path);
-
-            if (!in.is_open()) {
-                spdlog::get("console")->error("File not opened");
-                return false;
-            }
-
-            std::unordered_map<std::string, size_t> known_labels;
-            reverse_labels_.clear();
-
-            std::string line;
-
-            std::vector<Scalar> rowCache;
-            labels_.clear();
-
-            size_t label_idx = 0;
-
-            while (std::getline(in, line)) {
-                if (line.empty()) { //Orig if( !line.size() ) {
-                    continue;
-                }
-
-                rowCache.clear();
-
-                const char *ptr = line.c_str();
-                size_t len = line.length();
-
-                const char *start = ptr;
-                for (size_t i = 0; i < len; ++i) {
-
-                    if (ptr[i] == ',') {
-                        rowCache.push_back(std::atof(start));
-                        start = ptr + i + 1;
-                    }
-                }
-
-                const std::string label_str(start);
-
-                auto r = known_labels.find(start);
-
-                size_t found_label = label_idx;
-
-                if (r == known_labels.end()) {
-                    known_labels.insert(std::make_pair(label_str, label_idx));
-                    reverse_labels_.insert(std::make_pair(label_idx, label_str));
-
-                    spdlog::get("console")->info("Found new label {0}",label_str);
-
-                    ++label_idx;
-                } else {
-                    found_label = r->second;
-                }
-
-                if (!cols_) {
-                    cols_ = rowCache.size();
-                } else {
-
-                    if (cols_ != rowCache.size()) {
-                        spdlog::get("console")->error("Corrupted line \"{0}\"", line);
-                        spdlog::get("console")->error("Row size = {0} line size = {1}", cols_, rowCache.size());
-
-                        continue;
-                    }
-                }
-
-                VectorType col_vector(rowCache.size());
-                for (size_t i = 0; i < rowCache.size(); ++i) {
-                    col_vector(i) = rowCache[i];
-                }
-
-                data_.emplace_back(col_vector);
-                labels_.push_back(found_label);
-
-                ++rows_;
-            }
-
-            in.close();
-
-            assert(data_.size() == labels_.size());
-
-            return true;
-        }
+        Dataset(std::vector<VectorType> data)
+        :
+        data_(data),
+        labels_(data.size(),0),
+        rows_(data.size()),
+        cols_(data[0].size())
+        {}
 
         std::vector<VectorType>& data() {
             return data_;
