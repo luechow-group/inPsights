40c2e5c562e60ea14cf6f4a8ad052c32ed28698a
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-09-14 17:58:32 +0200

    Fixed error.

--- src/Executables/StringMethodGUI.cpp
+++ src/Executables/StringMethodGUI.cpp
@@ -76,7 +76,7 @@ int main(int argc, char *argv[]) {
 
     std::cout << ecA << std::endl;
 
-    ecB.permute(0, 1);
+    ecB.positionsVector().slice({0,2}).permute(Eigen::PermutationMatrix<Eigen::Dynamic>(Eigen::VectorXi{1,0}));
     std::cout << ecB << std::endl;
     //ecB.permute(8, 17);//Ethane
 
