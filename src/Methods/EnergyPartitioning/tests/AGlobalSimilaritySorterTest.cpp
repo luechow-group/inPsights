f78d3f7b31f6fbe39dfd919668eba97be9c534ac
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-10-17 00:28:34 +0200

    Adapted tests to missing SimilarReference and permutations.

--- src/Methods/EnergyPartitioning/tests/AGlobalSimilaritySorterTest.cpp
+++ src/Methods/EnergyPartitioning/tests/AGlobalSimilaritySorterTest.cpp
@@ -14,69 +14,82 @@ using namespace testing;
 class AGlobalSimilaritySorterTest : public ::testing::Test {
 public:
     std::vector<Reference> maxima;
-
+    std::vector<Sample> samples;
+    Eigen::VectorXd ekin;
     void SetUp() override {
-        auto rng = std::default_random_engine {};
+        ekin.resize(2);
+        ekin[0] = 0;
+        ekin[1] = 0;
 
         maxima = {
-                {1.00, ElectronsVector({{Spin::alpha,{0,0,1.00}},{Spin::beta,{0,0,0}}}),0},
-                {1.01, ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::beta,{0,0,1.01}}}),1},
-                {1.02, ElectronsVector({{Spin::alpha,{0,0,1.02}},{Spin::beta,{0,0,0}}}),2},
-                {1.03, ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::beta,{0,0,1.03}}}),3},
-                {1.10, ElectronsVector({{Spin::alpha,{0,0,1.10}},{Spin::beta,{0,0,0}}}),4},
-                {1.11, ElectronsVector({{Spin::alpha,{0,0,1.11}},{Spin::beta,{0,0,0}}}),5},
-                {1.12, ElectronsVector({{Spin::alpha,{0,0,1.12}},{Spin::beta,{0,0,0}}}),6},
-                {1.13, ElectronsVector({{Spin::alpha,{0,0,1.13}},{Spin::beta,{0,0,0}}}),7}
+                {1.00, ElectronsVector({{Spin::alpha,{0,0,1.00}},{Spin::beta,{0,0,0}}}), 0},
+                {1.01, ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::beta,{0,0,1.01}}}), 1},
+                {1.02, ElectronsVector({{Spin::alpha,{0,0,1.02}},{Spin::beta,{0,0,0}}}), 2},
+                {1.03, ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::beta,{0,0,1.03}}}), 3},
+                {1.10, ElectronsVector({{Spin::alpha,{0,0,1.10}},{Spin::beta,{0,0,0}}}), 4},
+                {1.11, ElectronsVector({{Spin::alpha,{0,0,1.11}},{Spin::beta,{0,0,0}}}), 5},
+                {1.12, ElectronsVector({{Spin::alpha,{0,0,1.12}},{Spin::beta,{0,0,0}}}), 6},
+                {1.13, ElectronsVector({{Spin::alpha,{0,0,1.13}},{Spin::beta,{0,0,0}}}), 7}
+        };
+        samples = {
+                {ElectronsVector({{Spin::alpha,{0,0,1.00}},{Spin::beta,{0,0,0}}}), ekin},
+                {ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::beta,{0,0,1.01}}}), ekin},
+                {ElectronsVector({{Spin::alpha,{0,0,1.02}},{Spin::beta,{0,0,0}}}), ekin},
+                {ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::beta,{0,0,1.03}}}), ekin},
+                {ElectronsVector({{Spin::alpha,{0,0,1.10}},{Spin::beta,{0,0,0}}}), ekin},
+                {ElectronsVector({{Spin::alpha,{0,0,1.11}},{Spin::beta,{0,0,0}}}), ekin},
+                {ElectronsVector({{Spin::alpha,{0,0,1.12}},{Spin::beta,{0,0,0}}}), ekin},
+                {ElectronsVector({{Spin::alpha,{0,0,1.13}},{Spin::beta,{0,0,0}}}), ekin}
         };
     }
 };
 
 TEST_F(AGlobalSimilaritySorterTest, OneList) {
     std::vector<SimilarReferences> similarReferencesVector;
-    GlobalSimilaritySorter globalSimilaritySorter(maxima, similarReferencesVector, 1);
+    GlobalSimilaritySorter globalSimilaritySorter(samples, maxima, similarReferencesVector, 1);
     globalSimilaritySorter.sort();
 
     ASSERT_EQ((*similarReferencesVector.at(0).repRefIt_).id_,0);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(0).it_).id_,1);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(1).it_).id_,2);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(2).it_).id_,3);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(3).it_).id_,4);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(4).it_).id_,5);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(5).it_).id_,6);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(6).it_).id_,7);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(0)).id_,1);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(1)).id_,2);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(2)).id_,3);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(3)).id_,4);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(4)).id_,5);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(5)).id_,6);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(6)).id_,7);
 }
 
 
 TEST_F(AGlobalSimilaritySorterTest, TwoLists) {
     std::vector<SimilarReferences> similarReferencesVector;
-    GlobalSimilaritySorter globalSimilaritySorter(maxima, similarReferencesVector, 0.1);
+    GlobalSimilaritySorter globalSimilaritySorter(samples, maxima, similarReferencesVector, 0.1);
     globalSimilaritySorter.sort();
 
     ASSERT_EQ((*similarReferencesVector.at(0).repRefIt_).id_,0);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(0).it_).id_,1);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(1).it_).id_,2);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(2).it_).id_,3);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(0)).id_,1);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(1)).id_,2);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(2)).id_,3);
 
     ASSERT_EQ((*similarReferencesVector.at(1).repRefIt_).id_,4);
-    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(0).it_).id_,5);
-    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(1).it_).id_,6);
-    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(2).it_).id_,7);
+    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(0)).id_,5);
+    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(1)).id_,6);
+    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(2)).id_,7);
 }
 
 TEST_F(AGlobalSimilaritySorterTest, TwoListsIncrementBorderCase) {
     std::vector<SimilarReferences> similarReferencesVector;
-    GlobalSimilaritySorter globalSimilaritySorter(maxima, similarReferencesVector, 0.02);
+    GlobalSimilaritySorter globalSimilaritySorter(samples, maxima, similarReferencesVector, 0.02);
     globalSimilaritySorter.sort();
 
     ASSERT_EQ((*similarReferencesVector.at(0).repRefIt_).id_,0);
-    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(0).it_).id_,1);
+    ASSERT_EQ((*similarReferencesVector.at(0).similarReferences_.at(0)).id_,1);
 
     ASSERT_EQ((*similarReferencesVector.at(1).repRefIt_).id_,2);
-    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(0).it_).id_,3);
+    ASSERT_EQ((*similarReferencesVector.at(1).similarReferences_.at(0)).id_,3);
 
     ASSERT_EQ((*similarReferencesVector.at(2).repRefIt_).id_,4);
-    ASSERT_EQ((*similarReferencesVector.at(2).similarReferences_.at(0).it_).id_,5);
+    ASSERT_EQ((*similarReferencesVector.at(2).similarReferences_.at(0)).id_,5);
 
     ASSERT_EQ((*similarReferencesVector.at(3).repRefIt_).id_,6);
-    ASSERT_EQ((*similarReferencesVector.at(3).similarReferences_.at(0).it_).id_,7);
+    ASSERT_EQ((*similarReferencesVector.at(3).similarReferences_.at(0)).id_,7);
 }
