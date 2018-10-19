4076311c4c7a2e52910bddedfa902316c00205a1
Michael Heuer <heuer@pc.rwth-aachen.de>
2018-10-19 17:33:55 +0200

    WIP

---
+++ src/Methods/EnergyPartitioning/tests/AReferenceTest.cpp
@@ -0,0 +1,17 @@
+//
+// Created by heuer on 19.10.18.
+//
+
+#include <gmock/gmock.h>
+#include <Reference.h>
+
+using namespace testing;
+
+class AReferenceTest : public ::testing::Test {
+public:
+};
+
+
+TEST_F(AReferenceTest, name){
+
+}
---
+++ src/Methods/EnergyPartitioning/tests/ASimilarReferencesTest.cpp
@@ -0,0 +1,18 @@
+//
+// Created by heuer on 19.10.18.
+//
+
+#include <gmock/gmock.h>
+#include <Reference.h>
+
+using namespace testing;
+
+class ASimilarReferencesTest : public ::testing::Test {
+public:
+};
+
+
+TEST_F(ASimilarReferencesTest, PermuteAll){
+
+    // TEST PERMUTEALL METHOD
+}
