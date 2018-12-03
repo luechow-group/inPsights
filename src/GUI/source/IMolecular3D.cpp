2d50eb3380d64aece271d6c4f2a0f44acfa1798f
Michael Heuer <heuer@pc.rwth-aachen.de>
2018-12-03 11:58:25 +0100

    WIP: Deleted unused IMolecular.

--- src/GUI/source/IMolecular3D.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by heuer on 30.11.18.
-//
-
-#include <IMolecular3D.h>
-
-IMolecular3D::IMolecular3D(MoleculeWidget *moleculeWidgetParent)
-        : moleculeWidgetParent_(moleculeWidgetParent)
-{}
