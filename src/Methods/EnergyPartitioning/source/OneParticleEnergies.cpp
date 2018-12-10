9885ecc1df65250c270becbe49a10559ce190556
Michael Heuer <heuer@pc.rwth-aachen.de>
2018-12-10 18:52:33 +0100

    Fixed major bug in oneParticleEnergies methods.
    An = operator should have been an += operator.

--- src/Methods/EnergyPartitioning/source/OneParticleEnergies.cpp
+++ src/Methods/EnergyPartitioning/source/OneParticleEnergies.cpp
@@ -8,9 +8,9 @@
 
 Eigen::VectorXd
 OneParticleEnergies::oneAtomEnergies(const IntraParticlesStatistics& Vnn, const ClusterData &clusterData) {
-    auto nElectrons = clusterData.VenStats_.rows();
+    //auto nElectrons = clusterData.VenStats_.rows(); //TODO delete?
     auto nAtoms = clusterData.VenStats_.cols();
-    const auto& Ven = clusterData.VenStats_;
+    //const auto& Ven = clusterData.VenStats_; //TODO delete?
 
     Eigen::VectorXd En = Eigen::VectorXd::Zero(nAtoms);
 
@@ -20,17 +20,17 @@ OneParticleEnergies::oneAtomEnergies(const IntraParticlesStatistics& Vnn, const 
             En[l] += 0.5 * Vnn.mean()(k, l);
         }
 
-        for (Eigen::Index  i = 0; i < nElectrons; ++i)
-            En[k] += 0.5 * Ven.mean()(i,k);
+        //for (Eigen::Index  i = 0; i < nElectrons; ++i)
+        //    En[k] += 0.5 * Ven.mean()(i,k); //TODO delete?
     }
     return En;
 }
 
 Eigen::VectorXd
 OneParticleEnergies::oneAtomEnergiesErrors(const IntraParticlesStatistics &Vnn, const ClusterData &clusterData) {
-    auto nElectrons = clusterData.VenStats_.rows();
+    //auto nElectrons = clusterData.VenStats_.rows();
     auto nAtoms = clusterData.VenStats_.cols();
-    const auto& Ven = clusterData.VenStats_;
+    //const auto& Ven = clusterData.VenStats_;
 
     Eigen::VectorXd EnErr = Eigen::VectorXd::Zero(nAtoms);
 
@@ -40,8 +40,8 @@ OneParticleEnergies::oneAtomEnergiesErrors(const IntraParticlesStatistics &Vnn, 
             EnErr[l] = 0.5 * std::sqrt(std::pow(EnErr[l], 2) + std::pow(Vnn.standardError()(k, l), 2));
         }
 
-        for (Eigen::Index  i = 0; i < nElectrons; ++i)
-            EnErr[k] = 0.5 * std::sqrt(std::pow(EnErr[k], 2) + std::pow(Ven.standardError()(i,k), 2));
+        //for (Eigen::Index  i = 0; i < nElectrons; ++i)
+        //    EnErr[k] = 0.5 * std::sqrt(std::pow(EnErr[k], 2) + std::pow(Ven.standardError()(i,k), 2)); //TODO delete?
     }
     return EnErr;
 }
@@ -56,10 +56,11 @@ Eigen::VectorXd OneParticleEnergies::oneElectronEnergies(const ClusterData &clus
     Eigen::VectorXd Ee = Eigen::VectorXd::Zero(nElectrons);
 
     for (Eigen::Index i = 0; i < nElectrons; ++i) {
-        Ee[i] = Te.mean()[i];
+        Ee[i] += Te.mean()[i];
 
         for (Eigen::Index k = 0; k < nAtoms; ++k)
-            Ee[i] += 0.5 * Ven.mean()(i, k);
+            Ee[i] += Ven.mean()(i, k);
+            //Ee[i] += 0.5 * Ven.mean()(i, k); //TODO delete?
 
         for (Eigen::Index  j = i + 1; j < nElectrons; ++j) {
             Ee[i] += 0.5 * Vee.mean()(i, j);
@@ -79,15 +80,19 @@ Eigen::VectorXd OneParticleEnergies::oneElectronEnergiesErrors(const ClusterData
     Eigen::VectorXd EeErr = Eigen::VectorXd::Zero(nElectrons);
 
     for (Eigen::Index i = 0; i < nElectrons; ++i) {
-        EeErr[i] = Te.standardError()[i];
+        EeErr[i] += std::pow(Te.standardError()[i], 2);
 
         for (Eigen::Index  k = 0; k < nAtoms; ++k)
-            EeErr[i] = 0.5*std::sqrt(std::pow(EeErr[i], 2) + std::pow(Ven.standardError()(i,k), 2));
+            EeErr[i] +=  std::pow(Ven.standardError()(i,k), 2);
+            //EeErr[i] = 0.5*std::sqrt(std::pow(EeErr[i], 2) + std::pow(Ven.standardError()(i,k), 2)); //TODO delete?
 
         for (Eigen::Index  j = i + 1; j < nElectrons; ++j) {
-            EeErr[i] = 0.5 * std::sqrt(std::pow(EeErr[i], 2) + std::pow(Vee.standardError()(i, j), 2));
-            EeErr[j] = 0.5 * std::sqrt(std::pow(EeErr[j], 2) + std::pow(Vee.standardError()(i, j), 2));
+            EeErr[i] += std::pow(0.5*Vee.standardError()(i, j), 2);
+            EeErr[j] += std::pow(0.5*Vee.standardError()(i, j), 2);
         }
     }
+
+    EeErr.cwiseSqrt();
+
     return EeErr;
 }
