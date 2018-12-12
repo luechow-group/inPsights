d169bff9c0704eb50378c8a1aa251efea554c8ec
Michael Heuer <heuer@pc.rwth-aachen.de>
2018-12-12 10:33:58 +0100

    Reformatting and removal of unnecessary includes.

--- src/Methods/EnergyPartitioning/include/EnergyCalculator.h
+++ src/Methods/EnergyPartitioning/include/EnergyCalculator.h
@@ -5,19 +5,19 @@
 #ifndef INPSIGHTS_ENERGYCALCULATOR_H
 #define INPSIGHTS_ENERGYCALCULATOR_H
 
-#include <utility>
+#include "Reference.h"
+#include "SimilarReferences.h"
+#include "Sample.h"
 #include <Statistics.h>
-#include <Reference.h>
-#include <Sample.h>
 #include <Logger.h>
-#include <SimilarReferences.h>
+#include <utility>
 
-class EnergyCalculator{
+class EnergyCalculator {
 public:
 
-    struct TotalEnergies{
+    struct TotalEnergies {
         TotalEnergies()
-        : Te(0),Vee(0),Ven(0),Vnn(0){};
+                : Te(0), Vee(0), Ven(0), Vnn(0) {};
 
         double totalEnergy() const {
             return Te + Vee + Ven + Vnn;
@@ -26,26 +26,26 @@ public:
         double Te, Vee, Ven, Vnn;
     };
 
-    EnergyCalculator(const std::vector<Sample>& samples, AtomsVector atoms);
+    EnergyCalculator(const std::vector<Sample> &samples, AtomsVector atoms);
 
     TotalEnergies calculateTotalEnergies();
 
     unsigned long addReference(const Reference &reference);
 
-    void calculateStatistics(const std::vector<std::vector<SimilarReferences>>& clusteredGloballySimilarMaxima);
+    void calculateStatistics(const std::vector<std::vector<SimilarReferences>> &clusteredGloballySimilarMaxima);
 
     // selects nWanted structures and prints the statistic data
-    void printCluster(std::vector<ElectronsVector>& structures);
+    void printCluster(std::vector<ElectronsVector> &structures);
 
     YAML::Node getYamlNode();
 
     std::string getYamlDocumentString();
 
 private:
-    const std::vector<Sample>& samples_;
+    const std::vector<Sample> &samples_;
     AtomsVector atoms_;
 
-    SingleParticlesStatistics valueStats_,TeStats_;
+    SingleParticlesStatistics valueStats_, TeStats_;
     IntraParticlesStatistics SeeStats_, VeeStats_, VnnStats_;
     InterParticlesStatistics VenStats_;
 
