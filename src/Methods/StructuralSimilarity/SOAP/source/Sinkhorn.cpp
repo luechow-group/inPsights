c77076ea7746dddb8bd671dfe8508aa83705008f
Michael Heuer <heuer@pc.rwth-aachen.de>
2018-12-14 12:27:09 +0100

    Formatting of Sinkhorn algorithm.

--- src/Methods/StructuralSimilarity/SOAP/source/Sinkhorn.cpp
+++ src/Methods/StructuralSimilarity/SOAP/source/Sinkhorn.cpp
@@ -4,47 +4,46 @@
 
 #include "Sinkhorn.h"
 
-Eigen::MatrixXd Sinkhorn::Pgamma(const Eigen::MatrixXd &C, double gamma, double eps){
+Eigen::MatrixXd Sinkhorn::Pgamma(const Eigen::MatrixXd &C, double gamma, double eps) {
     auto N = C.rows();
     auto M = C.cols();
 
-    Eigen::VectorXd u = Eigen::VectorXd::Constant(N,1./double(N));
-    Eigen::VectorXd v = Eigen::VectorXd::Constant(M,1./double(M));
-    Eigen::VectorXd uOld(N),vOld(M);
-    Eigen::MatrixXd K(N,M);
+    Eigen::VectorXd u = Eigen::VectorXd::Constant(N, 1. / double(N));
+    Eigen::VectorXd v = Eigen::VectorXd::Constant(M, 1. / double(M));
+    Eigen::VectorXd uOld(N), vOld(M);
+    Eigen::MatrixXd K(N, M);
 
-    double aN = 1.0/double(N), aM=1.0/double(M);
+    double aN = 1.0 / double(N), aM = 1.0 / double(M);
     double uDeviation, vDeviation;
-    double err = pow(eps,2);
+    double err = std::pow(eps, 2);
 
-    double lambda = 1./gamma;
-    K = (lambda*(C-Eigen::MatrixXd::Ones(N,M))).array().exp();
+    double lambda = 1. / gamma;
+    K = (lambda * (C - Eigen::MatrixXd::Ones(N, M))).array().exp();
 
     do {
         uOld = u;
         uDeviation = 0.0;
 
-        for (Eigen::Index i=0; i<N; ++i) {
+        for (Eigen::Index i = 0; i < N; ++i) {
             u[i] = K.row(i) * v;
-            // at this point we can compute how far off unity we are
-            uDeviation += pow(aN - uOld[i]*u[i],2);
+            uDeviation += pow(aN - uOld[i] * u[i], 2);
         }
-        u = aN*u.array().cwiseInverse();
+        u = aN * u.array().cwiseInverse();
 
         vOld = v;
         vDeviation = 0.0;
 
-        for (Eigen::Index j=0; j<M; ++j) {
+        for (Eigen::Index j = 0; j < M; ++j) {
             v[j] = K.col(j).transpose() * u;
-            vDeviation += pow(aM -  vOld[j]*v[j],2);
+            vDeviation += pow(aM - vOld[j] * v[j], 2);
         }
-        v = aM*v.array().cwiseInverse();
+        v = aM * v.array().cwiseInverse();
 
-    } while ( uDeviation > err && vDeviation > err);
+    } while (uDeviation > err && vDeviation > err);
 
-    return K.cwiseProduct(u*v.transpose());
+    return K.cwiseProduct(u * v.transpose());
 }
 
 double Sinkhorn::distance(Eigen::MatrixXd correlationMatrix, double gamma, double eps) {
-    return (Pgamma(correlationMatrix,gamma,eps).transpose()*correlationMatrix).trace();
+    return (Pgamma(correlationMatrix, gamma, eps).transpose() * correlationMatrix).trace();
 }
