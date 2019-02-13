0164be7a62b059f852eeb67309d14e486ed16444
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2019-02-13 20:40:20 +0100

    Calculate totalEnergy in energy partitioning test.

--- src/Methods/MaximaProcessing/tests/AEnergyPartitioningTest.cpp
+++ src/Methods/MaximaProcessing/tests/AEnergyPartitioningTest.cpp
@@ -11,8 +11,46 @@
 
 using namespace testing;
 
-TEST(AEnergyPartitioningTest, TwoPairs){
+class AEnergyPartitioningTest : public ::testing::Test {
+public:
 
+    Eigen::VectorXd Te;
+    Eigen::MatrixXd Vee;
+    Eigen::MatrixXd Ven;
+    Eigen::MatrixXd Vnn;
+
+    double totalEnergy;
+
+    void SetUp() override {
+        Te = Eigen::VectorXd::Constant(4,0.01);
+        Vee = Eigen::MatrixXd::Constant(4,4,1);
+        Ven = Eigen::MatrixXd::Constant(4,2,100);
+        Vnn = Eigen::MatrixXd::Constant(2,2,10000);
+
+        auto ne = Ven.rows();
+        auto na = Ven.cols();
+
+        totalEnergy = 0;
+        for (int i = 0; i < ne; ++i) {
+            totalEnergy += Te(i);
+
+            for (int j = i+1; j < ne; ++j)
+                totalEnergy += Vee(i,j);
+
+            for (int k = 0; k < na; ++k)
+                totalEnergy += Ven(i,k);
+        }
+
+        for (int k = 0; k < na; ++k) {
+            for (int l = k+1; l < na; ++l)
+                totalEnergy += Vnn(k,l);
+        }
+
+    }
+};
+
+
+TEST_F(AEnergyPartitioningTest, TwoPairs){
 
     MolecularGeometry mol = {
             AtomsVector({{Element::H, {0, 0, 1}},
@@ -22,8 +60,6 @@ TEST(AEnergyPartitioningTest, TwoPairs){
                              {Spin::alpha, {0, 0,-1}},
                              {Spin::beta,  {0, 0,-1}},
                             })};
-
-
 
     Eigen::MatrixXb A(4, 4);
     //all pairs // Two pairs: (0-1,) (2-3)
@@ -43,10 +79,6 @@ TEST(AEnergyPartitioningTest, TwoPairs){
     ASSERT_THAT(motifs.motifVector_[1].atomIndices(), ElementsAre(1));
     ASSERT_EQ(motifs.motifVector_[1].type(), MotifType::Core);
 
-    Eigen::VectorXd Te = Eigen::VectorXd::Constant(4,0.01);
-    Eigen::MatrixXd Vee = Eigen::MatrixXd::Constant(4,4,1);
-    Eigen::MatrixXd Ven = Eigen::MatrixXd::Constant(4,2,100);
-    Eigen::MatrixXd Vnn = Eigen::MatrixXd::Constant(2,2,10000);
     auto motifEnergies = EnergyPartitioning::MotifBased::calculateInterationEnergies(motifs, Te, Vee, Ven, Vnn);
 
     Eigen::VectorXd intraExpected = Eigen::VectorXd::Zero(motifs.motifVector_.size());
@@ -57,9 +89,11 @@ TEST(AEnergyPartitioningTest, TwoPairs){
 
     ASSERT_TRUE(motifEnergies.first.isApprox(intraExpected));
     ASSERT_TRUE(motifEnergies.second.isApprox(interExpected));
+
+    ASSERT_EQ(motifEnergies.first.sum()+motifEnergies.second.sum(), totalEnergy);
 };
 
-TEST(AEnergyPartitioningTest, TripleAndIsolated){
+TEST_F(AEnergyPartitioningTest, TripleAndIsolated){
     MolecularGeometry mol = {
             AtomsVector({{Element::H, {0, 0, 1}},
                          {Element::H, {0, 0,-1}}}),
@@ -89,13 +123,7 @@ TEST(AEnergyPartitioningTest, TripleAndIsolated){
     ASSERT_THAT(motifs.motifVector_[2].atomIndices(), ElementsAre());
     ASSERT_EQ(motifs.motifVector_[2].type(), MotifType::Valence);
 
-
-    Eigen::VectorXd Te = Eigen::VectorXd::Constant(4,0.01);
-    Eigen::MatrixXd Vee = Eigen::MatrixXd::Constant(4,4,1);
-    Eigen::MatrixXd Ven = Eigen::MatrixXd::Constant(4,2,100);
-    Eigen::MatrixXd Vnn = Eigen::MatrixXd::Constant(2,2,10000);
     auto motifEnergies = EnergyPartitioning::MotifBased::calculateInterationEnergies(motifs, Te, Vee, Ven, Vnn);
-
 
     Eigen::VectorXd intraExpected = Eigen::VectorXd::Zero(motifs.motifVector_.size());
     intraExpected << 201.02, 100.01, 0.01;
@@ -108,5 +136,7 @@ TEST(AEnergyPartitioningTest, TripleAndIsolated){
 
     ASSERT_TRUE(motifEnergies.first.isApprox(intraExpected));
     ASSERT_TRUE(motifEnergies.second.isApprox(interExpected));
+
+    ASSERT_EQ(motifEnergies.first.sum()+motifEnergies.second.sum(), totalEnergy);
 };
 
