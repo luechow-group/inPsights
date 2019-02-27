83f5f37a8f42384b99a10399e82ceda6b19663dc
Michael Heuer <heuer@pc.rwth-aachen.de>
2019-02-27 16:23:30 +0100

    Forgot other test.

--- src/Methods/StructuralSimilarity/tests/AMotifsTest.cpp
+++ src/Methods/StructuralSimilarity/tests/AMotifsTest.cpp
@@ -15,7 +15,7 @@ public:
     void SetUp() override {
 
         MolecularGeometry molecule = {
-                AtomsVector({{Element::H, {0, 0, 1}},
+                AtomsVector({{Element::Li, {0, 0, 1}},
                              {Element::H, {0, 0,-1}}}),
                 ElectronsVector({{Spin::alpha, {0, 0, 1}},
                                  {Spin::beta,  {0, 0, 1}},
@@ -25,10 +25,10 @@ public:
 
 
         A = Eigen::MatrixXb(4, 4);
-        A << 0, 1, 1, 0, \
-             1, 0, 1, 0, \
-             1, 1, 0, 0, \
-             0, 0, 0, 0;
+        A << 0, 1, 0, 0, \
+             1, 0, 0, 0, \
+             0, 0, 0, 1, \
+             0, 0, 1, 0;
 
         motifs = Motifs(A, molecule);
     };
@@ -40,11 +40,11 @@ TEST_F(AMotifsTest, Sorting) {
     ASSERT_THAT(motifs.motifVector_[0].atomIndices(), ElementsAre(0));
     ASSERT_EQ(motifs.motifVector_[0].type(), MotifType::Core);
 
-    ASSERT_THAT(motifs.motifVector_[1].electronIndices(), ElementsAre(2));
+    ASSERT_THAT(motifs.motifVector_[1].electronIndices(), ElementsAre());
     ASSERT_THAT(motifs.motifVector_[1].atomIndices(), ElementsAre(1));
     ASSERT_EQ(motifs.motifVector_[1].type(), MotifType::Core);
 
-    ASSERT_THAT(motifs.motifVector_[2].electronIndices(), ElementsAre(3));
+    ASSERT_THAT(motifs.motifVector_[2].electronIndices(), ElementsAre(2,3));
     ASSERT_THAT(motifs.motifVector_[2].atomIndices(), ElementsAre());
     ASSERT_EQ(motifs.motifVector_[2].type(), MotifType::Valence);
 }
