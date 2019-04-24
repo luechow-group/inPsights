b8b04e5e71796b1d8b7fe1a2df4f2c1ca26e15e6
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2019-04-24 20:48:05 +0200

    Added copyright notices.

--- src/Methods/MaximaProcessing/source/IdentityClusterer.cpp
+++ src/Methods/MaximaProcessing/source/IdentityClusterer.cpp
@@ -1,6 +1,19 @@
-//
-// Created by heuer on 12.12.18.
-//
+/* Copyright (C) 2018-2019 Michael Heuer.
+ *
+ * This file is part of inPsights.
+ * inPsights is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * inPsights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with inPsights. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <IdentityClusterer.h>
 #include <DistanceClusterer.h>
