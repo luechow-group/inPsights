2035c6fa6c574b674392687dfdd4b5b670c07d68
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2019-07-24 10:04:29 +0200

    Use cross product from the eigen lib.

--- src/Methods/SurfaceGeneration/include/MathUtils.h
+++ src/Methods/SurfaceGeneration/include/MathUtils.h
@@ -3,6 +3,7 @@
 #define QuickHull_MathUtils_hpp
 
 #include <Eigen/Core>
+#include <Eigen/Geometry>
 #include "Ray.h"
 
 namespace quickhull {
@@ -27,20 +28,8 @@ namespace quickhull {
 		        const Eigen::Matrix<T,3,1>& a,
 		        const Eigen::Matrix<T,3,1>& b,
 		        const Eigen::Matrix<T,3,1>& c) {
-			// We want to get (a-c).crossProduct(b-c) without constructing temp vectors
-			T x = a.x() - c.x();
-			T y = a.y() - c.y();
-			T z = a.z() - c.z();
-			T rhsx = b.x() - c.x();
-			T rhsy = b.y() - c.y();
-			T rhsz = b.z() - c.z();
-			T px = y * rhsz - z * rhsy ;
-			T py = z * rhsx - x * rhsz ;
-			T pz = x * rhsy - y * rhsx ;
-			return Eigen::Matrix<T,3,1>(px,py,pz);
+		    return (a-c).cross(b-c);
 		}
-		
-		
 	}
 	
 }
