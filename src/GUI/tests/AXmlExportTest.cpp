27ceefc5a9e2963bbe5f4124773364a88ac4429b
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2019-10-23 01:22:27 +0200

    Fixed warning and renamed test.

--- src/GUI/tests/AXmlExportTest.cpp
+++ src/GUI/tests/AXmlExportTest.cpp
@@ -29,23 +29,20 @@ public:
     };
 };
 
-TEST_F(AXmlExportTest, out) {
+TEST_F(AXmlExportTest, XmlTestFile) {
 
     X3domConverter x3Dconverter("test.html");
-
-    Qt3DCore::QEntity *root;
-
     QVector3D
         a = {0,-1,1},
         b = {1,2,1};
 
-    Cylinder cylinder1(root, Qt::green,
+    Cylinder cylinder1(nullptr, Qt::green,
                       {a,b},
                       0.2,
                       0.5);
 
-    Sphere s1(root, Qt::red, a, 0.2, 0.7);
-    Sphere s2(root, Qt::blue, b, 0.2, 0.3);
+    Sphere s1(nullptr, Qt::red, a, 0.2, 0.7);
+    Sphere s2(nullptr, Qt::blue, b, 0.2, 0.3);
 
     x3Dconverter.addCylinder(cylinder1);
     x3Dconverter.addSphere(s1);
