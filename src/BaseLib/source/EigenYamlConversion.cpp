7ad6b2cc320cb4bf4582d3d46b504fbe27d21186
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2019-11-04 16:37:32 +0100

    Added methods to convert Eigen::MatrixXd and triangular matrices.

--- src/BaseLib/source/EigenYamlConversion.cpp
+++ src/BaseLib/source/EigenYamlConversion.cpp
@@ -85,7 +85,7 @@ namespace YAML {
     Node convert<Eigen::VectorXd>::encode(const Eigen::VectorXd &rhs) {
         Node node;
 
-        for (long i = 0; i < rhs.size(); ++i)
+        for (Eigen::Index i = 0; i < rhs.size(); ++i)
             node.push_back(rhs[i]);
 
         return node;
@@ -103,8 +103,103 @@ namespace YAML {
     }
     Emitter &operator<<(Emitter &out, const Eigen::VectorXd &v) {
         out << Flow << BeginSeq;
-        for (long i = 0; i < v.size(); ++i) out << v[i];
+        for (Eigen::Index i = 0; i < v.size(); ++i) out << v[i];
         out << EndSeq;
+        return out;
+    }
+
+    Node convert<Eigen::MatrixXd>::encode(const Eigen::MatrixXd &rhs) {
+        Node node;
+
+        for (Eigen::Index i = 0; i < rhs.rows(); ++i) {
+            node["."] = 0; // Trick to get a map
+            node.remove(".");
+            for (Eigen::Index j = 0; j < rhs.cols(); ++j) {
+                node[i][j] = rhs(i,j);
+                node[i]["."] = 0; // Trick to get a map
+                node[i].remove(".");
+            }
+        }
+
+        return node;
+    }
+    bool convert<Eigen::MatrixXd>::decode(const Node &node, Eigen::MatrixXd &rhs) {
+
+        if (!node.IsMap() || !node[0].IsMap())
+            return false;
+
+        rhs.resize(node.size(),node[0].size());
+
+        for (Eigen::Index i = 0; i < rhs.rows(); ++i){
+            for (Eigen::Index j = 0; j < rhs.cols(); ++j){
+                rhs(i,j) = node[i][j].as<double>();
+            }
+        }
+
+        return true;
+    }
+    Emitter &operator<<(Emitter &out, const Eigen::MatrixXd &rhs) {
+
+        out << BeginMap;
+        for (Eigen::Index i = 0; i < rhs.rows(); ++i) {
+            out << Key << i << Value << BeginMap;
+            for (Eigen::Index j = 0; j < rhs.cols(); ++j) {
+                out << Key << j << Value << rhs(i,j);
+            }
+            out << EndMap;
+        }
+        out << EndMap;
+        return out;
+    }
+
+    Node convert<TriangularMatrixXd>::encode(
+            const TriangularMatrixXd &rhs) {
+        Node node;
+
+        Eigen::MatrixXd mat(rhs);
+
+        for (Eigen::Index i = 0; i < mat.rows()-1; ++i) {
+            node["."] = 0; // Trick to get a map
+            node.remove(".");
+            for (Eigen::Index j = i+1; j < mat.cols(); ++j) {
+                node[i][j] = mat(i,j);
+                node[i]["."] = 0; // Trick to get a map
+                node[i].remove(".");
+            }
+        }
+
+        return node;
+    }
+    bool convert<TriangularMatrixXd>::decode(const Node &node, TriangularMatrixXd &rhs) {
+
+        if (!node.IsMap() || !node[0].IsMap())
+            return false;
+
+        auto& mat = rhs.nestedExpression();
+        mat = Eigen::MatrixXd::Zero(node.size()+1,node[0].size()+1);
+
+        for (Eigen::Index i = 0; i < mat.rows()-1; ++i){
+            for (Eigen::Index j = i+1; j < mat.cols(); ++j){
+                mat(i,j) = node[i][j].as<double>();
+            }
+        }
+
+        return true;
+    }
+    Emitter &operator<<(Emitter &out, const TriangularMatrixXd &rhs) {
+
+        auto& mat = rhs.nestedExpression();
+
+        out << BeginMap;
+        for (Eigen::Index i = 0; i < mat.rows()-1; ++i) {
+            out << Key << i << Value << BeginMap;
+            for (Eigen::Index j = i+1; j < mat.cols(); ++j) {
+                out << Flow << Key << j << Value << mat(i,j);
+            }
+            out << EndMap;
+        }
+        out << EndMap;
+
         return out;
     }
 }
