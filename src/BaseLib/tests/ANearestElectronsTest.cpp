684269d1a0f335eb64d85d1dbd88728b5c1d1302
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2020-02-17 17:08:00 +0100

    Refactored test to use matchers.

--- src/BaseLib/tests/ANearestElectronsTest.cpp
+++ src/BaseLib/tests/ANearestElectronsTest.cpp
@@ -58,32 +58,26 @@ public:
 
 TEST_F(ANearestElectronsTest, GetNonValenceIndices) {
     std::list<long> indices = NearestElectrons::getNonValenceIndices(electrons, nuclei[0]);
-    std::list<long> reference = std::list<long>({0, 4});
-    ASSERT_EQ(reference, indices);
+    ASSERT_THAT(indices, ElementsAre(0,4));
 
     indices = NearestElectrons::getNonValenceIndices(electrons, nuclei[1]);
-    reference = std::list<long>({});
-    ASSERT_EQ(reference, indices);
+    ASSERT_THAT(indices,ElementsAre());
 };
 
 TEST_F(ANearestElectronsTest, GetNonValenceIndicesAll) {
     std::list<long> indices = NearestElectrons::getNonValenceIndices(electrons, nuclei);
-    std::list<long> reference({0, 4});
-    ASSERT_EQ(reference, indices);
+    ASSERT_THAT(indices, ElementsAre(0,4));
 };
 
-TEST_F(ANearestElectronsTest, GetElectronsByPosition) {
+TEST_F(ANearestElectronsTest, GetElectronsByPositionInverted) {
     Eigen::Vector3d position = TestMolecules::inbetween(nuclei, {0, 2}, 0.7);
     std::list<long> indices = NearestElectrons::getNearestElectronsIndices(electrons, position, 2);
-    std::list<long> reference({2, 6});
-    ASSERT_EQ(reference, indices);
+    ASSERT_THAT(indices, ElementsAre(2,6));
 };
 
 TEST_F(ANearestElectronsTest, GetValenceByPosition) {
-    std::list<long> indices = getNearestElectronsIndices(electrons, nuclei,
-                                                         nuclei[0].position(), 2);
-    std::list<long> reference({6, 7});
-    ASSERT_EQ(reference, indices);
+    std::list<long> indices = getNearestElectronsIndices(electrons, nuclei,nuclei[0].position(), 2);
+    ASSERT_THAT(indices, ElementsAre(6,7));
 };
 
 TEST_F(ANearestElectronsTest, PickElements) {
