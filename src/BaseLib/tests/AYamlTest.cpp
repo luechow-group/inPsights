e76087b52930bad47b047d7e651cade7f06df676
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2020-08-10 10:51:28 +0200

    Fixed failing yaml test from commit 0b549e.

--- src/BaseLib/tests/AYamlTest.cpp
+++ src/BaseLib/tests/AYamlTest.cpp
@@ -36,6 +36,6 @@ TEST(AYamlTest, JsonCompatibility){
     out << YAML::EndMap;
 
     std::string outstring = out.c_str();
-    std::string expected ="{\"Molecule\": {\"Atoms\": {\"Types\": [\"He\", \"H\"],\n  \"Positions\": [\n      [0, 0, 0.37],\n      [0, 0, -0.37],\n      ]}\n\"Electrons\": {\"Types\": [\"a\", \"a\", \"b\"],\n  \"Positions\": [\n      [0, 0, 0.37],\n      [0, 0, 0.37],\n      [0, 0, -0.37],\n      ]}}}";
+    std::string expected ="{\"Molecule\": {\"Atoms\": {\"Types\": [\"He\", \"H\"],\n  \"Positions\": [\n      [0, 0, 0.37],\n      [0, 0, -0.37],\n      ]},\n\"Electrons\": {\"Types\": [\"a\", \"a\", \"b\"],\n  \"Positions\": [\n      [0, 0, 0.37],\n      [0, 0, 0.37],\n      [0, 0, -0.37],\n      ]}}}";
     ASSERT_EQ(outstring,expected);
 }
