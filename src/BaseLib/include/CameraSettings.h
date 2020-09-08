0d1ee4d566860e28eb9ed116a230c4967cc24d4f
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2020-09-08 12:41:27 +0200

    Replace GPL 3.0 and BSD-2-Clause-views header with SPDX identifier (see https://spdx.org/licenses)

--- src/BaseLib/include/CameraSettings.h
+++ src/BaseLib/include/CameraSettings.h
@@ -1,20 +1,6 @@
-/* Copyright (C) 2019 Michael Heuer.
- * Copyright (C) 2020 Leonard Reuter.
- *
- * This file is part of inPsights.
- * inPsights is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * inPsights is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with inPsights. If not, see <https://www.gnu.org/licenses/>.
- */
+// Copyright (C) 2019 Michael Heuer.
+// Copyright (C) 2020 Leonard Reuter.
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef INPSIGHTS_CAMERASETTINGS_H
 #define INPSIGHTS_CAMERASETTINGS_H
