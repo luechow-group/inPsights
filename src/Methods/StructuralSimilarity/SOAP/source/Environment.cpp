86d3cf76fcb0f2052d2e35026697730b0a588943
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2020-10-28 19:22:35 +0100

    Refactored environment.

--- src/Methods/StructuralSimilarity/SOAP/source/Environment.cpp
+++ src/Methods/StructuralSimilarity/SOAP/source/Environment.cpp
@@ -3,41 +3,55 @@
 
 #include "Environment.h"
 #include "Cutoff.h"
-#include "SOAPSettings.h"
 #include <SpecialMathFunctions/BoostSphericalHarmonics.h>
+
+#include <utility>
 
 using namespace SOAP;
 
-SphericalCoordinates::SphericalCoordinates(const Eigen::Vector3d& vec)
-        : r(0),theta(0),phi(0) {
+SphericalCoordinates::SphericalCoordinates(const Eigen::Vector3d &vec)
+        : r(0), theta(0), phi(0) {
     BoostSphericalHarmonics::toSphericalCoordsStandardizedWith2PiShift(vec, r, theta, phi);
 }
 
-Environment::Environment(MolecularGeometry molecularGeometry, EnumeratedType<int> enumeratedType)
-: molecularGeometry_(std::move(molecularGeometry)),
-  enumeratedType_(std::move(enumeratedType)){}
+Environment::Environment(const MolecularGeometry &molecularGeometry, Eigen::Vector3d center)
+        : molecularGeometry_(molecularGeometry),
+          ownIdx_(-1), // this influences the behaviour of selectParticle()
+          agnosticQ_(General::settings.mode() == General::Mode::typeAgnostic),
+          center_(std::move(center)) {}
 
-std::vector<std::pair<Particle<int>,SphericalCoordinates>> Environment::selectParticles(int expansionTypeId) const {
-    auto [foundQ, idx] = molecularGeometry_.findIndexByEnumeratedType(enumeratedType_);
-    assert(foundQ && "The enumerated type must exist.");
+Environment::Environment(const MolecularGeometry &molecularGeometry, EnumeratedType<int> enumeratedType)
+        : molecularGeometry_(molecularGeometry),
+          ownIdx_(getOwnIndex(molecularGeometry, enumeratedType)),
+          agnosticQ_(General::settings.mode() == General::Mode::typeAgnostic),
+          center_(molecularGeometry.positions()[ownIdx_]) {}
 
-    auto center = molecularGeometry_[idx].position();
+long Environment::getOwnIndex(const MolecularGeometry &molecularGeometry, EnumeratedType<int> enumeratedType) {
+    assert(molecularGeometry.findIndexByEnumeratedType(enumeratedType).first && "The enumerated type must exist.");
+    return molecularGeometry.findIndexByEnumeratedType(enumeratedType).second;
+}
 
-    auto mode = General::settings.mode();
-    std::vector<std::pair<Particle<int>,SphericalCoordinates>> selectedParticles;
+std::vector<std::pair<Particle<int>, SphericalCoordinates>> Environment::selectParticles(int expansionTypeId) const {
+    std::vector<std::pair<Particle<int>, SphericalCoordinates>> selectedParticles;
 
     for (unsigned i = 0; i < unsigned(molecularGeometry_.numberOfEntities()); ++i) {
         const auto &neighbor = molecularGeometry_[i];
 
-        if( i != idx && (neighbor.type() == expansionTypeId || mode == General::Mode::typeAgnostic) ) {
-
-            SphericalCoordinates sphericalCoords(neighbor.position()-center);
+        if (selectParticleQ(i, neighbor, expansionTypeId)) {
+            SphericalCoordinates sphericalCoords(neighbor.position() - center_);
 
             if (Cutoff::withinCutoffRadiusQ(sphericalCoords.r)) {
-                selectedParticles.emplace_back<std::pair<Particle<int>,SphericalCoordinates>>(
-                        {neighbor,sphericalCoords});
+                selectedParticles.emplace_back<std::pair<Particle<int>, SphericalCoordinates>>(
+                        {neighbor, sphericalCoords});
             }
         }
     }
     return selectedParticles;
+}
+
+bool Environment::selectParticleQ(unsigned int index, const TypedParticle &neighbor, int expansionTypeId) const {
+    if(neighbor.type() == expansionTypeId || agnosticQ_) // type is ok
+        return index != ownIdx_; // not the same particle // TODO change this?
+    else
+        return false;
 }
--- src/Methods/StructuralSimilarity/SOAP/tests/ALocalSimilarityTest.cpp
+++ src/Methods/StructuralSimilarity/SOAP/tests/ALocalSimilarityTest.cpp
@@ -17,30 +17,31 @@ class ALocalSimilarityTest : public ::testing::Test {
 public:
 
     MolecularGeometry molecule = TestMolecules::CO2::nuclei;
-    double eps = std::numeric_limits<double>::epsilon()*1e3;
+    double eps = std::numeric_limits<double>::epsilon() * 1e3;
+
     void SetUp() override {
         spdlog::set_level(spdlog::level::off);
     };
 };
 
-TEST_F(ALocalSimilarityTest , GenericNormalization) {
+TEST_F(ALocalSimilarityTest, GenericNormalization) {
     ParticleKit::create(molecule);
     General::settings.mode = General::Mode::typeAgnostic;
 
-    Environment e1(molecule, EnumeratedType<int>(int(Element::O),0));
-    Environment e2(molecule, EnumeratedType<int>(int(Element::O),1));
+    Environment e1(molecule, EnumeratedType<int>(int(Element::O), 0));
+    Environment e2(molecule, EnumeratedType<int>(int(Element::O), 1));
 
     ASSERT_NEAR(LocalSimilarity::kernel(e1, e1), 1.0, eps);
     ASSERT_NEAR(LocalSimilarity::kernel(e2, e2), 1.0, eps);
 };
 
-TEST_F(ALocalSimilarityTest , SameEnvironmentsOnDifferentCenters) {
+TEST_F(ALocalSimilarityTest, SameEnvironmentsOnDifferentCenters) {
     ParticleKit::create(molecule);
 
-    Environment e1(molecule, EnumeratedType<int>(int(Element::O),0));
-    Environment e2(molecule, EnumeratedType<int>(int(Element::O),1));
+    Environment e1(molecule, EnumeratedType<int>(int(Element::O), 0));
+    Environment e2(molecule, EnumeratedType<int>(int(Element::O), 1));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e1, e2),1.0, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e1, e2), 1.0, eps);
 };
 
 TEST_F(ALocalSimilarityTest, TwoDifferentParticlesOnSameCenter) {
@@ -50,27 +51,27 @@ TEST_F(ALocalSimilarityTest, TwoDifferentParticlesOnSameCenter) {
     Radial::settings.nmax = 2;
     Angular::settings.lmax = 2;
 
-    Environment e1(mol, EnumeratedType<int>(int(Spin::alpha),1));
-    Environment e2(mol, EnumeratedType<int>(int(Spin::beta),0));
+    Environment e1(mol, EnumeratedType<int>(int(Spin::alpha), 1));
+    Environment e2(mol, EnumeratedType<int>(int(Spin::beta), 0));
 
-    ASSERT_LT(LocalSimilarity::kernel(e1, e2),1.0-eps);
+    ASSERT_LT(LocalSimilarity::kernel(e1, e2), 1.0 - eps);
 };
 
-TEST_F(ALocalSimilarityTest , Cross) {
+TEST_F(ALocalSimilarityTest, Cross) {
     ParticleKit::create(molecule);
     General::settings.mode = General::Mode::typeAgnostic;
 
-    Environment e0(molecule, EnumeratedType<int>(int(Element::C),0));
-    Environment e1(molecule, EnumeratedType<int>(int(Element::O),0));
-    Environment e2(molecule, EnumeratedType<int>(int(Element::O),1));
+    Environment e0(molecule, EnumeratedType<int>(int(Element::C), 0));
+    Environment e1(molecule, EnumeratedType<int>(int(Element::O), 0));
+    Environment e2(molecule, EnumeratedType<int>(int(Element::O), 1));
 
     auto val = LocalSimilarity::kernel(e0, e1);
-    ASSERT_LT(val,1.0);
-    ASSERT_GT(val,0.0);
+    ASSERT_LT(val, 1.0);
+    ASSERT_GT(val, 0.0);
 
     auto val2 = LocalSimilarity::kernel(e0, e2);
-    ASSERT_LE(val2,1.0);
-    ASSERT_GE(val2,0.0);
+    ASSERT_LE(val2, 1.0);
+    ASSERT_GE(val2, 0.0);
 
     ASSERT_NEAR(LocalSimilarity::kernel(e0, e1), LocalSimilarity::kernel(e0, e2), eps);
 };
@@ -80,31 +81,31 @@ TEST_F(ALocalSimilarityTest, TypeSpecificNormalization) {
     ParticleKit::create(molecule);
     General::settings.mode = General::Mode::chemical;
 
-    Environment e1(molecule, EnumeratedType<int>(int(Element::O),0));
-    Environment e2(molecule, EnumeratedType<int>(int(Element::O),1));
+    Environment e1(molecule, EnumeratedType<int>(int(Element::O), 0));
+    Environment e2(molecule, EnumeratedType<int>(int(Element::O), 1));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e1, e1),1.0, eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e2, e2),1.0, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e1, e1), 1.0, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e2, e2), 1.0, eps);
 };
 
 TEST_F(ALocalSimilarityTest, SameEnvironmentOnDifferentCentersGeneric) {
     ParticleKit::create(molecule);
     General::settings.mode = General::Mode::typeAgnostic;
 
-    Environment e1(molecule, EnumeratedType<int>(int(Element::O),0));
-    Environment e2(molecule, EnumeratedType<int>(int(Element::O),1));
+    Environment e1(molecule, EnumeratedType<int>(int(Element::O), 0));
+    Environment e2(molecule, EnumeratedType<int>(int(Element::O), 1));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e1, e2),1.0, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e1, e2), 1.0, eps);
 };
 
 TEST_F(ALocalSimilarityTest, SameEnvironmentOnDifferentCentersTypeSpecific) {
     ParticleKit::create(molecule);
     General::settings.mode = General::Mode::chemical;
 
-    Environment e1(molecule, EnumeratedType<int>(int(Element::O),0));
-    Environment e2(molecule, EnumeratedType<int>(int(Element::O),1));
+    Environment e1(molecule, EnumeratedType<int>(int(Element::O), 0));
+    Environment e2(molecule, EnumeratedType<int>(int(Element::O), 1));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e1, e2),1.0, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e1, e2), 1.0, eps);
 };
 
 TEST_F(ALocalSimilarityTest, IsolatedSpecies) {
@@ -116,8 +117,8 @@ TEST_F(ALocalSimilarityTest, IsolatedSpecies) {
     Environment e1(isolated, molecule.findEnumeratedTypeByIndex(1));
     Environment e2(isolated, molecule.findEnumeratedTypeByIndex(2));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1),1.0, eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e2, e0),1.0, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1), 1.0, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e2, e0), 1.0, eps);
 };
 
 TEST_F(ALocalSimilarityTest, H2sameCenter) {
@@ -126,10 +127,10 @@ TEST_F(ALocalSimilarityTest, H2sameCenter) {
 
     auto H2 = TestMolecules::H2::ElectronsInCores::normal;
 
-    Environment a0(H2, EnumeratedType<int>(int(Element::H),0));
-    Environment e0(H2, EnumeratedType<int>(int(Spin::alpha),0));
+    Environment a0(H2, EnumeratedType<int>(int(Element::H), 0));
+    Environment e0(H2, EnumeratedType<int>(int(Spin::alpha), 0));
 
-    ASSERT_LT(LocalSimilarity::kernel(a0, e0),1.0-eps);
+    ASSERT_LT(LocalSimilarity::kernel(a0, e0), 1.0 - eps);
 }
 
 TEST_F(ALocalSimilarityTest, twoOppositeElectrons) {
@@ -138,10 +139,10 @@ TEST_F(ALocalSimilarityTest, twoOppositeElectrons) {
     auto eaeb = TestMolecules::twoElectrons::oppositeSpin;
     ParticleKit::create(eaeb);
 
-    Environment e0(eaeb, EnumeratedType<int>(int(Spin::alpha),0));
-    Environment e1(eaeb, EnumeratedType<int>(int(Spin::beta),0));
+    Environment e0(eaeb, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment e1(eaeb, EnumeratedType<int>(int(Spin::beta), 0));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1),0.0,eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1), 0.0, eps);
 }
 
 TEST_F(ALocalSimilarityTest, twoOppositeElectronsReversedOrder) {
@@ -150,10 +151,10 @@ TEST_F(ALocalSimilarityTest, twoOppositeElectronsReversedOrder) {
     auto ebea = TestMolecules::twoElectrons::oppositeSpinReversedOrder;
     ParticleKit::create(ebea);
 
-    Environment e0(ebea, EnumeratedType<int>(int(Spin::beta),0));
-    Environment e1(ebea, EnumeratedType<int>(int(Spin::alpha),0));
+    Environment e0(ebea, EnumeratedType<int>(int(Spin::beta), 0));
+    Environment e1(ebea, EnumeratedType<int>(int(Spin::alpha), 0));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1),0.0,eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1), 0.0, eps);
 }
 
 TEST_F(ALocalSimilarityTest, twoOppositeSpinElectronsComparision) {
@@ -163,11 +164,11 @@ TEST_F(ALocalSimilarityTest, twoOppositeSpinElectronsComparision) {
 
     General::settings.mode = General::Mode::chemical;
 
-    Environment mol1e0(mol1, EnumeratedType<int>(int(Spin::alpha),0));
-    Environment mol1e1(mol1, EnumeratedType<int>(int(Spin::beta),0));
+    Environment mol1e0(mol1, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment mol1e1(mol1, EnumeratedType<int>(int(Spin::beta), 0));
 
-    Environment mol2e0(mol2, EnumeratedType<int>(int(Spin::beta),0));
-    Environment mol2e1(mol2, EnumeratedType<int>(int(Spin::alpha),0));
+    Environment mol2e0(mol2, EnumeratedType<int>(int(Spin::beta), 0));
+    Environment mol2e1(mol2, EnumeratedType<int>(int(Spin::alpha), 0));
 
     ASSERT_NEAR(LocalSimilarity::kernel(mol1e0, mol1e1), 0.0, eps);
     ASSERT_NEAR(LocalSimilarity::kernel(mol2e0, mol2e1), 0.0, eps);
@@ -202,10 +203,10 @@ TEST_F(ALocalSimilarityTest, twoAlphaElectrons) {
     auto eaea = TestMolecules::twoElectrons::sameSpinAlpha;
     ParticleKit::create(eaea);
 
-    Environment e0(eaea, EnumeratedType<int>(int(Spin::alpha),0));
-    Environment e1(eaea, EnumeratedType<int>(int(Spin::alpha),1));
+    Environment e0(eaea, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment e1(eaea, EnumeratedType<int>(int(Spin::alpha), 1));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1),1.0,eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1), 1.0, eps);
 }
 
 
@@ -215,10 +216,10 @@ TEST_F(ALocalSimilarityTest, twoBetaElectrons) {
     auto ebeb = TestMolecules::twoElectrons::sameSpinBeta;
     ParticleKit::create(ebeb);
 
-    Environment e0(ebeb, EnumeratedType<int>(int(Spin::beta),0));
-    Environment e1(ebeb, EnumeratedType<int>(int(Spin::beta),1));
+    Environment e0(ebeb, EnumeratedType<int>(int(Spin::beta), 0));
+    Environment e1(ebeb, EnumeratedType<int>(int(Spin::beta), 1));
 
-    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1),1.0,eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1), 1.0, eps);
 }
 
 TEST_F(ALocalSimilarityTest, ChemicalAndAlchemicalComparison) {
@@ -227,11 +228,11 @@ TEST_F(ALocalSimilarityTest, ChemicalAndAlchemicalComparison) {
     ParticleKit::create(mol1);
 
     General::settings.mode = General::Mode::chemical;
-    Environment mol1e0(mol1, EnumeratedType<int>(int(Spin::alpha),0));
-    Environment mol1e1(mol1, EnumeratedType<int>(int(Spin::beta),0));
+    Environment mol1e0(mol1, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment mol1e1(mol1, EnumeratedType<int>(int(Spin::beta), 0));
 
-    Environment mol2e0(mol2, EnumeratedType<int>(int(Spin::beta),0));
-    Environment mol2e1(mol2, EnumeratedType<int>(int(Spin::alpha),0));
+    Environment mol2e0(mol2, EnumeratedType<int>(int(Spin::beta), 0));
+    Environment mol2e1(mol2, EnumeratedType<int>(int(Spin::alpha), 0));
 
     ASSERT_NEAR(LocalSimilarity::kernel(mol1e0, mol1e1), 0.0, eps);
     ASSERT_NEAR(LocalSimilarity::kernel(mol2e0, mol2e1), 0.0, eps);
@@ -240,7 +241,7 @@ TEST_F(ALocalSimilarityTest, ChemicalAndAlchemicalComparison) {
     ASSERT_NEAR(LocalSimilarity::kernel(mol2e0, mol1e1), 1.0, eps);
 
 
-    General::settings.pairSimilarities[{int(Spin::alpha),int(Spin::beta)}] = 0.5;
+    General::settings.pairSimilarities[{int(Spin::alpha), int(Spin::beta)}] = 0.5;
     General::settings.mode = General::Mode::alchemical;
     auto simMol1e0e1 = LocalSimilarity::kernel(mol1e0, mol1e1);
     auto simMol2e0e1 = LocalSimilarity::kernel(mol2e0, mol2e1);
@@ -251,7 +252,7 @@ TEST_F(ALocalSimilarityTest, ChemicalAndAlchemicalComparison) {
     ASSERT_GT(simMol2e0e1, 0.0);
     ASSERT_LT(simMol2e0e1, 1.0);
 
-    ASSERT_EQ(simMol1e0e1,simMol2e0e1);
+    ASSERT_EQ(simMol1e0e1, simMol2e0e1);
 
     ASSERT_NEAR(LocalSimilarity::kernel(mol1e0, mol2e1), 1.0, eps);
     ASSERT_NEAR(LocalSimilarity::kernel(mol2e0, mol1e1), 1.0, eps);
@@ -263,26 +264,26 @@ TEST_F(ALocalSimilarityTest, DissociationIntoTwoIsolatedSpecies) {
     General::settings.mode = General::Mode::chemical;
     Cutoff::settings.radius = 2;// bohr
     Cutoff::settings.width = 1;// bohr // the inner plateau ends at 1
-    ParticleKit::create({},{2,0}); //the particle kit consists of two alpha electrons
+    ParticleKit::create({}, {2, 0}); //the particle kit consists of two alpha electrons
 
     // alter !both! environments by moving the second electron
-    unsigned steps = 5  ;
+    unsigned steps = 5;
     double rmax = 3; // bohr (cutoff is at 8 bohr)
 
     for (unsigned i = 0; i < steps; ++i) {
-        double r = rmax*double(i)/double(steps-1);
+        double r = rmax * double(i) / double(steps - 1);
 
-        MolecularGeometry mol= {AtomsVector(),
-                                ElectronsVector({{Spin::alpha, {0,0,0}},
-                                                 {Spin::alpha, {0,0,r}}
+        MolecularGeometry mol = {AtomsVector(),
+                                 ElectronsVector({{Spin::alpha, {0, 0, 0}},
+                                                  {Spin::alpha, {0, 0, r}}
                                                  })};
 
-        Environment e0(mol, EnumeratedType<int>(int(Spin::alpha),0));
-        Environment e1(mol, EnumeratedType<int>(int(Spin::alpha),1));
+        Environment e0(mol, EnumeratedType<int>(int(Spin::alpha), 0));
+        Environment e1(mol, EnumeratedType<int>(int(Spin::alpha), 1));
 
-        ASSERT_EQ(LocalSimilarity::kernel(e0,e0),1);
-        ASSERT_EQ(LocalSimilarity::kernel(e1,e1),1);
-        ASSERT_EQ(LocalSimilarity::kernel(e0,e1),1);
+        ASSERT_EQ(LocalSimilarity::kernel(e0, e0), 1);
+        ASSERT_EQ(LocalSimilarity::kernel(e1, e1), 1);
+        ASSERT_EQ(LocalSimilarity::kernel(e0, e1), 1);
     }
 };
 
@@ -290,43 +291,53 @@ TEST_F(ALocalSimilarityTest, DissociationIntoOneIsolatedSpecies) {
     General::settings.mode = General::Mode::typeAgnostic;
     Cutoff::settings.radius = 2;// bohr
     Cutoff::settings.width = 1;// bohr
-    ParticleKit::create({},{3,0}); //the particle kit consists of three alpha electrons
+    ParticleKit::create({}, {3, 0}); //the particle kit consists of three alpha electrons
 
     MolecularGeometry mol;
-    mol= {AtomsVector(),ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::alpha,{0,0,0}},{Spin::alpha, {0,0,-0.1}}})};
-    Environment e0(mol, EnumeratedType<int>(int(Spin::alpha),0));
-    Environment e1(mol, EnumeratedType<int>(int(Spin::alpha),1));
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e0),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e1,e1),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e1),1,eps);
+    mol = {AtomsVector(), ElectronsVector({{Spin::alpha, {0, 0, 0}},
+                                           {Spin::alpha, {0, 0, 0}},
+                                           {Spin::alpha, {0, 0, -0.1}}})};
+    Environment e0(mol, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment e1(mol, EnumeratedType<int>(int(Spin::alpha), 1));
+    ASSERT_NEAR(LocalSimilarity::kernel(e0, e0), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e1, e1), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e0, e1), 1, eps);
 
-    mol= {AtomsVector(),ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::alpha,{0,0,0.5}},{Spin::alpha, {0,0,-0.1}}})};
-    e0 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),0));
-    e1 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),1));
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e0),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e1,e1),1,eps);
-    ASSERT_GT(LocalSimilarity::kernel(e0,e1),0);
-    ASSERT_LT(LocalSimilarity::kernel(e0,e1),1);
+    mol = {AtomsVector(), ElectronsVector({{Spin::alpha, {0, 0, 0}},
+                                           {Spin::alpha, {0, 0, 0.5}},
+                                           {Spin::alpha, {0, 0, -0.1}}})};
+    Environment e2(mol, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment e3(mol, EnumeratedType<int>(int(Spin::alpha), 1));
+    ASSERT_NEAR(LocalSimilarity::kernel(e2, e2), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e3, e3), 1, eps);
+    ASSERT_GT(LocalSimilarity::kernel(e2, e3), 0);
+    ASSERT_LT(LocalSimilarity::kernel(e2, e3), 1);
 
-    mol= {AtomsVector(),ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::alpha,{0,0,1.5}},{Spin::alpha, {0,0,-0.1}}})};
-    e0 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),0));
-    e1 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),1));
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e0),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e1,e1),1,eps);
-    ASSERT_GT(LocalSimilarity::kernel(e0,e1),0);
-    ASSERT_LT(LocalSimilarity::kernel(e0,e1),1);
+    mol = {AtomsVector(), ElectronsVector({{Spin::alpha, {0, 0, 0}},
+                                           {Spin::alpha, {0, 0, 1.5}},
+                                           {Spin::alpha, {0, 0, -0.1}}})};
+    Environment e4(mol, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment e5(mol, EnumeratedType<int>(int(Spin::alpha), 1));
+    ASSERT_NEAR(LocalSimilarity::kernel(e4, e4), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e5, e5), 1, eps);
+    ASSERT_GT(LocalSimilarity::kernel(e4, e5), 0);
+    ASSERT_LT(LocalSimilarity::kernel(e4, e5), 1);
 
-    mol= {AtomsVector(),ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::alpha,{0,0,2.0}},{Spin::alpha, {0,0,-0.1}}})};
-    e0 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),0));
-    e1 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),1));
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e0),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e1,e1),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e1),0,eps);
+    mol = {AtomsVector(), ElectronsVector({{Spin::alpha, {0, 0, 0}},
+                                           {Spin::alpha, {0, 0, 2.0}},
+                                           {Spin::alpha, {0, 0, -0.1}}})};
+    Environment e6(mol, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment e7(mol, EnumeratedType<int>(int(Spin::alpha), 1));
+    ASSERT_NEAR(LocalSimilarity::kernel(e6, e6), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e7, e7), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e6, e7), 0, eps);
 
-    mol= {AtomsVector(),ElectronsVector({{Spin::alpha,{0,0,0}},{Spin::alpha,{0,0,2.5}},{Spin::alpha, {0,0,-0.1}}})};
-    e0 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),0));
-    e1 = Environment(mol, EnumeratedType<int>(int(Spin::alpha),1));
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e0),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e1,e1),1,eps);
-    ASSERT_NEAR(LocalSimilarity::kernel(e0,e1),0,eps);
+    mol = {AtomsVector(), ElectronsVector({{Spin::alpha, {0, 0, 0}},
+                                           {Spin::alpha, {0, 0, 2.5}},
+                                           {Spin::alpha, {0, 0, -0.1}}})};
+    Environment e8(mol, EnumeratedType<int>(int(Spin::alpha), 0));
+    Environment e9(mol, EnumeratedType<int>(int(Spin::alpha), 1));
+    ASSERT_NEAR(LocalSimilarity::kernel(e8, e8), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e9, e9), 1, eps);
+    ASSERT_NEAR(LocalSimilarity::kernel(e8, e9), 0, eps);
 };
