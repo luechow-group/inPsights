9a0b11f4f1295f96adffec64b6461d7d518ae0ca
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2020-10-30 15:26:31 +0100

    Changes to CompareMolecule.

--- src/Executables/CompareMolecules.cpp
+++ src/Executables/CompareMolecules.cpp
@@ -2,11 +2,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <iostream>
+#include <iomanip>
 #include <yaml-cpp/yaml.h>
 #include <MolecularGeometry.h>
-#include <StructuralSimilarity.h>
+
 #include <ParticleKit.h>
 #include <SOAPSettings.h>
+#include <MolecularSpectrum.h>
+#include <StructuralSimilarity.h>
+#include <LocalSimilarity.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -27,34 +31,72 @@ int main(int argc, char *argv[]) {
     if (soapSettingsNode[SOAP::Cutoff::settings.name()])
         SOAP::Cutoff::settings = Settings::SOAP::Cutoff(soapSettingsNode);
 
-    auto geometriesNode = doc["Geometries"];
-
     AtomKit akit;
     ElectronKit ekit;
-
     std::vector<MolecularGeometry> mols;
-    for (const auto& node : geometriesNode) {
-        auto mol = MolecularGeometry(
-                node["Atoms"].as<AtomsVector>(),
-                node["Electrons"].as<ElectronsVector>());
+
+    std::vector<AtomsVector> atomsCollection;
+
+    for(auto geomNode : doc["Geometries"]) {
+        AtomsVector atoms;
+        ElectronsVector electrons;
+
+        if(geomNode["File"].IsDefined()) {
+            auto molDoc = YAML::LoadAllFromFile(geomNode["File"]["name"].as<std::string>())[1];
+            atoms = molDoc["Atoms"].as<AtomsVector>();
+            auto clusterIds = geomNode["File"]["clusterIds"].as<std::vector<unsigned>>();
+            
+            for (auto clusterId : clusterIds) {
+                electrons = molDoc["Clusters"][clusterId]["Structures"][0].as<ElectronsVector>();
+                //electrons = molDoc["Clusters"][clusterId]["SampleAverage"].as<ElectronsVector>();
+                mols.emplace_back(MolecularGeometry({}, electrons));
+                //mols.emplace_back(MolecularGeometry(atoms, electrons));
+                atomsCollection.emplace_back(atoms);
+            }
+        } else if (geomNode["Manual"].IsDefined()){
+            atoms = geomNode["Manual"]["Atoms"].as<AtomsVector>();
+            electrons = geomNode["Manual"]["Electrons"].as<ElectronsVector>();
+
+            mols.emplace_back(MolecularGeometry({}, electrons));
+            //mols.emplace_back(MolecularGeometry(atoms, electrons));
+            atomsCollection.emplace_back(atoms);
+        }
 
         akit = SOAP::ParticleKit::merge(
                 akit,
-                SOAP::ParticleKit::internal::createAtomKitFromAtomsVector(mol.atoms()));
+                SOAP::ParticleKit::internal::createAtomKitFromAtomsVector(atoms));
         ekit = SOAP::ParticleKit::merge(
                 ekit,
-                SOAP::ParticleKit::internal::createElectronKitFromElectronsVector(mol.electrons()));
-
-        mols.emplace_back(mol);
+                SOAP::ParticleKit::internal::createElectronKitFromElectronsVector(electrons));
     }
 
     SOAP::ParticleKit::create(akit, ekit);
 
-    for (std::vector<MolecularGeometry>::size_type i = 0; i < mols.size()-1; ++i) {
+    std::cout << std::setprecision(2) << std::endl;
+   for (std::vector<MolecularGeometry>::size_type i = 0; i < mols.size()-1; ++i) {
         for (std::vector<MolecularGeometry>::size_type j = i+1; j < mols.size(); ++j) {
             std::cout << i << " " << j << " "
-            << SOAP::StructuralSimilarity::kernel(mols[i],mols[j]) << std::endl;
+                      << SOAP::LocalSimilarity::kernel(
+                              SOAP::Environment(mols[i],(
+                                      atomsCollection[i][1].position()+atomsCollection[i][0].position()) / 2.0),
+                              SOAP::Environment(mols[j],(
+                                      atomsCollection[j][0].position()+atomsCollection[j][1].position()) / 2.0)
+                              ) << std::endl;
         }
+        std::cout << std::endl;
     }
 
+    std::cout << "Glob" << std::endl;
+
+    std::vector<SOAP::MolecularSpectrum> specs;
+    for(const auto& mol : mols)
+        specs.emplace_back(mol);
+
+    for (std::vector<MolecularGeometry>::size_type i = 0; i < specs.size()-1; ++i) {
+        for (std::vector<MolecularGeometry>::size_type j = i+1; j < specs.size(); ++j) {
+            std::cout << i << " " << j << " "
+            << SOAP::StructuralSimilarity::kernel(specs[i],specs[j]) << std::endl;
+        }
+        std::cout << std::endl;
+    }
 }
