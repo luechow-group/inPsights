978bfa26d9e468f4521aa5c19918ec4d322c183d
Leonard Reuter <reuter@pc.rwth-aachen.de>
2021-12-22 17:39:59 +0100

    Added directional light export to X3domConverter.

--- src/GUI/source/X3domConverter.cpp
+++ src/GUI/source/X3domConverter.cpp
@@ -2,19 +2,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <X3domConverter.h>
+#include <ToString.h>
 
 
 X3domConverter::X3domConverter(const std::string &filename,
                                const std::string& title,
-                               const std::string& comment) {
+                               const std::string& comment,
+                               const QVector3D& lightDirection) {
     file.open(filename);
-    startScene(title, comment);
+    startScene(title, comment, lightDirection);
 }
 X3domConverter::X3domConverter(const std::string &filename)
-    : X3domConverter(filename, filename, ""){}
+    : X3domConverter(filename, filename, "", QVector3D(0.0, 0.0, -1.0)){}
 
 
-void X3domConverter::startScene(const std::string& title, const std::string& comment){
+void X3domConverter::startScene(const std::string& title, const std::string& comment,
+                                const QVector3D& lightDirection) {
     file << "<html> \n"
             "<head> \n"
             "<title>" + title + "</title>\n"
@@ -27,7 +30,11 @@ void X3domConverter::startScene(const std::string& title, const std::string& com
             "<center> \n"
             "<x3d width='100%' height='100%'> \n"
             "<scene>\n"
-            "<DirectionalLight direction='0,0,-1' global='true'></DirectionalLight>\n";
+            "<DirectionalLight direction='"
+            + ToString::doubleToString(lightDirection.x(), 4, 0, false)
+            + ToString::doubleToString(lightDirection.y(), 4, 0, false)
+            + ToString::doubleToString(lightDirection.z(), 4, 0, false) +
+            "' global='true'></DirectionalLight>\n";
 };
 
 void X3domConverter::addSphere(const Sphere & sphere, unsigned sortKey){
